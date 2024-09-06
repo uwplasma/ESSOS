36890f7318fcbc2532c692d35ce9829e7d3fe9cf
EstevaoMGomes <estevao.gomes04@gmail.com>
2024-09-06 17:00:31 +0200

    Updated Optimization and Tracing Examples

--- examples/tracing_GC.py
+++ examples/tracing_GC.py
@@ -1,24 +1,29 @@
-import jax
-import sys
 import os
-from time import time
-import matplotlib.pyplot as plt
+os.mkdir("images") if not os.path.exists("images") else None
+os.mkdir("images/tracing") if not os.path.exists("images/tracing") else None
+os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=4'
+
+import sys
+sys.path.insert(1, os.path.dirname(os.getcwd()))
+
+import jax
 import jax.numpy as jnp
 from jax import grad
 
-os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=10'
 # Show on which platform JAX is running.
 print("JAX running on", [jax.devices()[i].platform.upper() for i in range(len(jax.devices()))])
 
-os.mkdir("images") if not os.path.exists("images") else None
+from time import time
+import matplotlib.pyplot as plt
 
 
-sys.path.insert(1, os.path.dirname(os.getcwd()))
 
-from ESSOS import CreateEquallySpacedCurves, Curves, Coils, Particles, optimize, loss, optimize_adam, projection2D, projection2D_top
+
+
+from ESSOS import CreateEquallySpacedCurves, Curves, Coils, Particles, projection2D, projection2D_top
 from MagneticField import B_norm, B
 
-model = "Lorentz" # "Guiding Center" or "Lorentz"
+model = "Guiding Center" # "Guiding Center" or "Lorentz"
 
 n_curves=2
 order=3
@@ -31,13 +36,13 @@ R = A*r
 
 r_init = r/4
 maxtime = 3e-5
-timesteps=15000
+timesteps = int(maxtime/5.0e-10)
 
-n_particles = len(jax.devices())
+n_particles = len(jax.devices()*2)
 particles = Particles(n_particles)
 
 dofs = jnp.reshape(jnp.array(
-    [5.890267840284325, 0.023015696788602182, 3.0157059371274517, -0.046787514040739324, 0.030158677016322175, 0.023729563724613666, 0.010555318529532919, 1.2003653254255195, 0.021229108610829883, 0.7054071640246989, -0.03393562054298491, -0.09097561376688454, 0.04491308553952268, -0.09660039344228535, 0.03346565357388103, -3.011851311084344, -0.0014506467708994709, 0.014675300276992076, 0.012068127915754798, 0.046991298948393295, 0.03500700615731799, 4.956795617354576, -0.05711561438660614, 2.4294629222993422, -0.026804562686284138, -0.04148356880312705, 0.0067459924660292, -0.00027898376776209653, 3.376085535692114, 0.6991845688079809, 1.719176069256245, -0.04299883638572471, -0.08076762907715977, -0.06997710316728854, 0.0037054411220158933, 0.001713667571414534, -3.0191643465872406, -0.01674095593209053, -0.008726490715081067, -0.014843704032555879, -0.045924289908639615, 0.03541769998164553]
+    [[[6.092399894957858, -0.024470340574007914, 3.0719746921121005, -0.028368927672627452, 0.0209969184912135, 0.047894031060180094, 0.25637378199180805], [0.8716042523881473, 0.05766079617231436, 0.5590378572065909, -0.18902743059120447, 0.15146435631733493, 0.03685179096003223, 0.557422958102676], [0.5045795016525154, -2.492316864969798, -0.3112614640391156, -0.012557962312413287, -0.4874554808123133, 0.25782973452019947, 0.4904250515493357]], [[4.699634400701741, 0.05433444026266678, 2.1102361473807227, -0.025550352547440258, -0.14693310973186055, 0.05046299854266632, 0.3816227741533424], [3.521371748567928, 0.014105103236513228, 1.5537123508542654, 0.0715558471890779, 0.20395300747959985, -0.023761103913984536, 0.35927656131445906], [0.06408331708629529, -2.5656461690839008, -0.4639042571658612, 0.5090810923281613, -0.10814277419357944, 0.5600433107883754, 0.5620951717018253]]]
 ), (n_curves, 3, 2*order+1))
 
 #curves = CreateEquallySpacedCurves(n_curves, order, R, r, nfp=4, stellsym=True)
@@ -72,42 +77,40 @@ print(f"Time to trace trajectories: {end-start:.2f} seconds")
 
 ########################################
 
-projection2D(R, r, r_init, trajectories, show=False, save_as="images/trajectories.pdf")
-projection2D_top(R, r, trajectories, show=False, save_as="images/top_trajectories.pdf")
+projection2D(R, r, r_init, trajectories, show=False, save_as="images/optimization/init_pol_trajectories.pdf")
+projection2D_top(R, r, trajectories, show=False, save_as="images/optimization/init_tor_trajectories.pdf")
 
 plt.figure()
 for i in range(len(trajectories)):
     if model == "Guiding Center":
         v_par = trajectories[i, :, 3]
-    elif model == "Lorentz":
+    else:
         B_field = jnp.apply_along_axis(B, 1, trajectories[i, :, :3], stel.gamma(), stel.currents)
         v_par = jnp.sum(trajectories[i, :, 3:]*B_field, axis=1)/jnp.linalg.norm(B_field, axis=1)
-    else:
-        raise ValueError("Model must be 'Guiding Center' or 'Lorentz'")
-    
     plt.plot(jnp.arange(jnp.size(trajectories, 1))*maxtime/timesteps, v_par)
+
 plt.title("Parallel Velocity")
 plt.xlabel("time [s]")
 plt.ylabel(r"parallel velocity [ms$^{-1}$]")
-plt.savefig("images/v_par.pdf", transparent=True)
+y_limit = max(jnp.abs(jnp.max(trajectories[:, :, 3])), jnp.abs(jnp.min(trajectories[:, :, 3])))
+plt.ylim(-1.2*y_limit, 1.2*y_limit)
+plt.savefig("images/tracing/v_par.pdf", transparent=True)
 
+y_limit = 0
 plt.figure()
 for i in range(len(trajectories)):
     normB = jnp.apply_along_axis(B_norm, 1, trajectories[i, :, :3], stel.gamma(), stel.currents)
     if model == "Guiding Center":
-        normalized_energy = (μ[i]*normB + 0.5*particles.mass*trajectories[i, :, 3]**2)/particles.energy
-    elif model == "Lorentz":
-        normalized_energy = 0.5*particles.mass*(jnp.sum(trajectories[i, :, 3:]**2, axis=1))/particles.energy
+        normalized_energy = (μ[i]*normB + 0.5*particles.mass*trajectories[i, :, 3]**2)/particles.energy-1
     else:
-        raise ValueError("Model must be 'Guiding Center' or 'Lorentz'")
+        normalized_energy = 0.5*particles.mass*(jnp.sum(trajectories[i, :, 3:]**2, axis=1))/particles.energy-1
     plt.plot(jnp.arange(jnp.size(trajectories, 1))*maxtime/timesteps, normalized_energy)
+    y_limit = max(y_limit, jnp.abs(jnp.max(normalized_energy)), jnp.abs(jnp.min(normalized_energy)))
+
 plt.title("Energy Conservation")
 plt.xlabel("time [s]")
-plt.ylabel(r"$\frac{E}{E_\alpha}$")
-y_limit = max(jnp.max(normalized_energy)-1, 1-jnp.min(normalized_energy))
-order = jnp.floor(jnp.log10(y_limit))
-plt.ylim(1-10**(order+1), 1+10**(order+1))
-plt.yticks(jnp.linspace(1-10**(order+1), 1+10**(order+1), 11))
-plt.savefig("images/energy_non_opt.pdf", transparent=True)
+plt.ylabel(r"$\frac{E-E_\alpha}{E_\alpha}$")
+plt.ylim(-y_limit, y_limit)
+plt.savefig("images/tracing/energy_non_opt.pdf", transparent=True)
 
-stel.plot(trajectories=trajectories, title="Stellator", save_as="images/stellator_non_opt.pdf", show=True)
+stel.plot(trajectories=trajectories, title="Stellator", save_as="images/tracing/stellator.pdf", show=True)
