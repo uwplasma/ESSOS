befa1fc6287c50438812d8f973735476fb8f2bb2
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2024-11-14 23:29:23 -0600

    Now with better optimization, outputting curves to vtu, focusing on trapped particles

--- examples/optimize_gc.py
+++ examples/optimize_gc.py
@@ -1,7 +1,6 @@
 import os
-os.mkdir("images") if not os.path.exists("images") else None
-os.mkdir("images/optimization") if not os.path.exists("images/optimization") else None
-os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=26'
+os.mkdir("output") if not os.path.exists("output") else None
+os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=24'
 
 import sys
 sys.path.insert(1, os.path.dirname(os.getcwd()))
@@ -19,32 +18,36 @@ from MagneticField import norm_B
 import matplotlib.pyplot as plt
 from time import time
 
+from simsopt.geo import CurveXYZFourier, curves_to_vtk
+from simsopt.field import particles_to_vtk
+
 n_curves=2
-order=5
+order=4
 
 A = 1.7 # Aspect ratio
 R = 6
 r = R/A
 r_init = r/3
-n_total_optimizations = 6
+n_total_optimizations = 14
 n_iterations_adam   = [5, 20, 30, 10]
 learning_rates_adam = [0.01, 0.005, 0.001, 0.0005]
-n_iteration_least_squares = 30
-ftol_least_squares = 1e-3
-advance_factor_each_optimization = 1.3
+n_iteration_least_squares = [20, 20, 20]
+diff_step_least_squares = [1e-2, 1e-3, 1e-4]
+ftol_least_squares = 1e-4
+advance_factor_each_optimization = 1.2
 
 model = "Guiding Center"
 
-maxtime = 2.3e-5 # seconds
-timesteps = int(maxtime*1e7)
+maxtime = 2.5e-5 # seconds
+timesteps = int(maxtime*1.1e7)
 
 particles = Particles(len(jax.devices()))
 
 # dofs = jnp.reshape(jnp.array(
-#     [[[5.89060390004571, -0.015150958534461115, 3.4591263438392446, 0.009699411716427936, 0.005537886410328967, 0.01955236062484789, -0.0021303528735225332, 0.0001238418385942917, 0.0011086450745253376, -0.002420205928959896, -0.0006756278019258931], [1.1793007949496566, 0.08004018778961734, 0.7029351106867904, -0.07094041261590181, 0.0020962549285922, -0.09580795350852979, 0.002692152068876774, -0.001971671169262141, 0.0015050178986789498, 0.011716003007586445, -0.0002192853422018339], [0.00925104968020525, -3.5337315253521644, -0.0006108384827500621, -0.004366106990217004, -0.005030258098696509, 0.001104679532083246, 9.417702560043752e-05, -0.000986852560245771, -0.00013184236797089718, 0.001498170896074649, -8.605289805770677e-05]], [[4.966928866203091, -0.020499263364279284, 2.9215033402400006, 0.030097757994063147, -0.001658376055921632, 0.05565323764723816, 0.002707143955919183, 0.001902227900558677, -0.0006014732428081339, -0.006951086409465891, -0.0015487952197048328], [3.3391470376526553, 0.03817293358747562, 1.9710511196219191, -0.0657504681460036, -0.008659372879836676, -0.07895161230412942, 0.002366128646258934, -0.0012328464807348071, 0.00120388490548888, 0.008955092767896359, -0.0008852913774149211], [0.02850918894375921, -3.5196447485566495, -0.003713297221886397, 0.005876325060884672, -0.01669711926998968, -0.0037687087970454496, 0.0036648895183505935, 0.00048733839516404377, 0.001556413876890159, 0.0023240807293555795, -0.0008712806280707237]]]
+#     [[[5.87861514784579, -0.28501866663349484, 3.4605189781659873, -0.12015624299307263, 0.021570557809795016, 0.011180132886233103, -0.005914068747621175, 0.034911113393685356, -0.2251164586817591, -0.48021484259221686, 0.0017654158055129938], [1.0658542864260812, 1.596040619988665, 0.565536458298842, 0.008660268749333927, -0.055656894617905996, -0.5943288470635641, -0.007504236106932222, 0.015126688042198883, 0.03629734685444535, 0.1406524187674465, -0.010631452790200351], [-0.12442634754918139, -3.4938686453340564, 0.0398564066737513, 0.0063662297101601956, 0.01397593917706845, 0.10356710656582224, -0.03663673656744568, -0.1302055966545474, -0.009540145756700466, -0.06783184015770215, -0.04386722025275589]], [[4.928538865238128, -0.8889703375010589, 2.9498631330089893, 0.004118912063285094, -0.0027167488153265534, 0.31069225476189133, -0.12072964005061287, 0.07013635477010735, -0.08818514845400854, -0.10438658509356558, 0.001015675716562636], [3.348184787512189, 1.3038766710921765, 1.891757654652235, -0.012860090424311877, 0.019435714929617435, -0.6940406707925618, -0.1362967646810659, 0.3497993853357873, -0.04575370296007795, -0.08763635202967397, -0.07427452292886186], [-0.015370703753422317, -3.525501559957153, 0.040276889036711075, 0.16315769641365546, -0.033099327566270216, 0.05300123362473358, 0.024936527119119065, -0.004079761449087372, -0.06293940543249064, -0.004823080906574666, -0.014828452800671684]]]
 # ), (n_curves, 3, 2*order+1))
 # curves = Curves(dofs, nfp=4, stellsym=True)
-# stel = Coils(curves, jnp.array([1.0, 1.202074057360835]))
+# stel = Coils(curves, jnp.array([1.0, 1.6315314795574953]))
 
 curves = CreateEquallySpacedCurves(n_curves, order, R, r, nfp=4, stellsym=True)
 stel = Coils(curves, jnp.array([1.0]*n_curves))
@@ -57,8 +60,8 @@ trajectories = stel.trace_trajectories(particles, initial_values, maxtime=maxtim
 print("Trajectories shape:", trajectories.shape)
 print(f"Time to trace trajectories: {time()-time0:.2f} seconds")
 
-projection2D(R, r, trajectories, show=False, save_as="images/optimization/init_pol_trajectories.pdf")
-projection2D_top(R, r, trajectories, show=False, save_as="images/optimization/init_tor_trajectories.pdf")
+projection2D(R, r, trajectories, show=False, save_as="output/init_pol_trajectories.pdf")
+projection2D_top(R, r, trajectories, show=False, save_as="output/init_tor_trajectories.pdf")
 
 plt.figure()
 for i in range(len(trajectories)):
@@ -68,7 +71,7 @@ plt.xlabel("time [s]")
 plt.ylabel(r"parallel velocity [ms$^{-1}$]")
 y_limit = max(jnp.abs(jnp.max(trajectories[:, :, 3])), jnp.abs(jnp.min(trajectories[:, :, 3])))
 plt.ylim(-1.2*y_limit, 1.2*y_limit)
-plt.savefig("images/optimization/init_vpar.pdf", transparent=True)
+plt.savefig("output/init_vpar.pdf", transparent=True)
 
 normB = jnp.apply_along_axis(norm_B, 0, initial_values[:3, :], stel.gamma, stel.gamma_dash, stel.currents)
 μ = particles.mass*initial_vperp**2/(2*normB)
@@ -84,11 +87,20 @@ plt.title("Energy Conservation")
 plt.xlabel("time [s]")
 plt.ylabel(r"$\frac{E-E_\alpha}{E_\alpha}$")
 plt.ylim(-1.2*y_limit, 1.2*y_limit)
-plt.savefig("images/optimization/init_energy.pdf", transparent=True)
+plt.savefig("output/init_energy.pdf", transparent=True)
 
-stel.plot(trajectories=trajectories, title="Initial Stellator", save_as="images/optimization/init_stellator.pdf", show=False)
+stel.plot(trajectories=trajectories, title="Initial Stellator", save_as="output/init_stellator.pdf", show=False)
 plt.close()
 
+def create_simsopt_curves(curves):
+    curves_simsopt = []
+    for i, curve in enumerate(curves):
+        curves_simsopt.append( CurveXYZFourier(100, order) )
+        curves_simsopt[i].x = jnp.ravel(curve)
+    return curves_simsopt
+
+curves_to_vtk(create_simsopt_curves(stel._curves), "output/curves_init", close=True)
+particles_to_vtk(res_tys=jnp.concatenate([trajectories[:, :, 3:4], trajectories[:, :, :3]], axis=2), filename="output/particles_init")
 ############################################################################################################
 
 start = time()
@@ -101,20 +113,25 @@ print(f"Loss function initial value: {loss_value:.8f}, took: {time()-start:.2f} 
 start = time()
 # grad_loss_value = grad(loss, argnums=0)(stel.dofs, stel.dofs_currents, stel, particles, R, r, initial_values, maxtime, timesteps)
 grad_loss_value = grad(loss)(dofs_with_currents, stel, particles, R, r, initial_values, maxtime, timesteps)
-print(f"Grad loss function initial value:\n{jnp.ravel(grad_loss_value)}")
+# print(f"Grad loss function initial value:\n{jnp.ravel(grad_loss_value)}")
 print(f"Grad shape: {grad_loss_value.shape}, took: {time()-start:.2f} seconds")
 
 start = time()
 for i in range(n_total_optimizations):
-    ## USING ADAM AND OPTAX
-    # for n_it, lr in zip(n_iterations_adam, learning_rates_adam):
-    #     print(f"Optimization {i+1} of {n_total_optimizations} with learning rate {lr}, {n_it} iterations, maxtime={maxtime}, timesteps={timesteps}")
-    #     res=optimize(stel, particles, R, r, initial_values, maxtime=maxtime, timesteps=timesteps, method={"method": "OPTAX adam", "learning_rate": lr, "iterations": n_it})
     ## USING SCIPY AND LEAST SQUARES
-    print(f"Optimization {i+1} of {n_total_optimizations} with {n_iteration_least_squares} iterations, ftol={ftol_least_squares}, maxtime={maxtime}, timesteps={timesteps}")
-    res=optimize(stel, particles, R, r, initial_values, maxtime=maxtime, timesteps=timesteps, method={"method": "least_squares", "ftol": ftol_least_squares, "max_nfev": n_iteration_least_squares})
+    for n_it, diff_step in zip(n_iteration_least_squares, diff_step_least_squares):
+        print(f"Optimization {i+1} of {n_total_optimizations} with {n_it} iterations, ftol={ftol_least_squares}, maxtime={maxtime}, timesteps={timesteps}, diff_step={diff_step}")
+        res=optimize(stel, particles, R, r, initial_values, maxtime=maxtime, timesteps=timesteps, method={"method": "least_squares", "ftol": ftol_least_squares, "max_nfev": n_it, "diff_step": diff_step})
+    # USING ADAM AND OPTAX
+    for n_it, lr in zip(n_iterations_adam, learning_rates_adam):
+        print(f"Optimization {i+1} of {n_total_optimizations} with learning rate {lr}, {n_it} iterations, maxtime={maxtime}, timesteps={timesteps}")
+        res=optimize(stel, particles, R, r, initial_values, maxtime=maxtime, timesteps=timesteps, method={"method": "OPTAX adam", "learning_rate": lr, "iterations": n_it})
     
-    stel.save_coils(f"Optimization_{i+1}.txt", text=f"loss={res}, maxtime={maxtime}, timesteps={timesteps}")
+    stel.save_coils(f"output/Optimization_{i+1}.txt", text=f"loss={res}, maxtime={maxtime}, timesteps={timesteps}")
+    curves_to_vtk(create_simsopt_curves(stel._curves), f"output/curves_{i+1}", close=True)
+    trajectories = stel.trace_trajectories(particles, initial_values, maxtime=maxtime, timesteps=timesteps)
+    particles_to_vtk(res_tys=jnp.concatenate([trajectories[:, :, 3:4], trajectories[:, :, :3]], axis=2), filename=f"output/particles_{i+1}")
+    
     maxtime *= advance_factor_each_optimization
     timesteps = int(timesteps*advance_factor_each_optimization)
 
@@ -124,8 +141,8 @@ stel.save_coils("Optimizations.txt")
 
 trajectories = stel.trace_trajectories(particles, initial_values, maxtime=maxtime, timesteps=timesteps)
 
-projection2D(R, r, trajectories, show=False, save_as="images/optimization/opt_pol_trajectories.pdf")
-projection2D_top(R, r, trajectories, show=False, save_as="images/optimization/opt_tor_trajectories.pdf")
+projection2D(R, r, trajectories, show=False, save_as="output/opt_pol_trajectories.pdf")
+projection2D_top(R, r, trajectories, show=False, save_as="output/opt_tor_trajectories.pdf")
 
 plt.figure()
 for i in range(len(trajectories)):
@@ -135,7 +152,7 @@ plt.xlabel("time [s]")
 plt.ylabel(r"parallel velocity [ms$^{-1}$]")
 y_limit = max(jnp.abs(jnp.max(trajectories[:, :, 3])), jnp.abs(jnp.min(trajectories[:, :, 3])))
 plt.ylim(-1.2*y_limit, 1.2*y_limit)
-plt.savefig("images/optimization/opt_vpar.pdf", transparent=True)
+plt.savefig("output/opt_vpar.pdf", transparent=True)
 
 normB = jnp.apply_along_axis(norm_B, 0, initial_values[:3, :], stel.gamma, stel.gamma_dash, stel.currents)
 μ = particles.mass*initial_vperp**2/(2*normB)
@@ -151,6 +168,9 @@ plt.title("Optimized Energy Conservation")
 plt.xlabel("time [s]")
 plt.ylabel(r"$\frac{E-E_\alpha}{E_\alpha}$")
 plt.ylim(-1.2*y_limit, 1.2*y_limit)
-plt.savefig("images/optimization/opt_energy.pdf", transparent=True)
+plt.savefig("output/opt_energy.pdf", transparent=True)
 
-stel.plot(show=True, trajectories=trajectories, title="Optimized Stellator", save_as="images/optimization/opt_stellator.pdf")
+stel.plot(show=True, trajectories=trajectories, title="Optimized Stellator", save_as="output/opt_stellator.pdf")
+
+curves_to_vtk(create_simsopt_curves(stel._curves), f"output/curves_opt", close=True)
+particles_to_vtk(res_tys=jnp.concatenate([trajectories[:, :, 3:4], trajectories[:, :, :3]], axis=2), filename="output/particles_opt")
