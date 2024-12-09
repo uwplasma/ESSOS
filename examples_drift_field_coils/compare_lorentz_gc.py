b35e7aef4df934454a1bef1f8a43adc3915371a5
eduardolneto <eduardo.lascasneto@gmail.com>
2024-12-09 17:51:09 +0000

    Testing some extra things
    Former-commit-id: e051decf3a28e79d4c600f67d88ff971496a11c8

---
+++ examples_drift_field_coils/compare_lorentz_gc.py
@@ -0,0 +1,205 @@
+import os
+import jax
+import sys
+from time import time
+import jax.numpy as jnp
+from matplotlib import cm
+import matplotlib.pyplot as plt
+number_of_cores = 10
+number_of_particles_per_core = 1
+os.environ["XLA_FLAGS"] = f'--xla_force_host_platform_device_count={number_of_cores}'
+print("JAX running on", len(jax.devices()), jax.devices()[0].platform.upper())
+sys.path.insert(1, os.path.dirname(os.getcwd()))
+from ESSOS import CreateEquallySpacedCurves, Coils, Particles
+from MagneticField import B, B_norm
+
+os.mkdir("images") if not os.path.exists("images") else None
+
+def set_axes_equal(ax):
+    """
+    Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    """
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+    
+def remove_3D_axes(ax):
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.set_zlabel('')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+    ax.grid(False)
+    ax.xaxis.pane.fill = False  # Hide the panes
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    ax.xaxis.pane.set_edgecolor('none')
+    ax.yaxis.pane.set_edgecolor('none')
+    ax.zaxis.pane.set_edgecolor('none')
+    ax.set_axis_off()
+    
+n_curves=2
+nfp=4
+order=2
+r = 3
+A = 2 # Aspect ratio
+R = A*r
+r_init = r/4
+maxtime = 1.0e-4
+timesteps_guiding_center=max(1000,int(maxtime/1.0e-8))
+timesteps_lorentz=int(maxtime/1.0e-10)
+nparticles = len(jax.devices())*number_of_particles_per_core
+n_segments=100
+coil_current = 7e6
+n_points_plot_L =  5000
+n_points_plot_gc = 1000
+
+particles = Particles(nparticles)
+
+curves = CreateEquallySpacedCurves(n_curves, order, R, r, nfp=nfp, stellsym=True)
+stel = Coils(curves, jnp.array([coil_current]*n_curves))
+
+# If there is a previous optimization, use the following x to set the dofs and currents
+x = [5.8816768669949235, -0.0001091781773779678, 2.9426538084165696, -0.0006121931796838491, 0.0005488831652268538, 1.1700514303927547, -3.846204946440075e-05, 0.5854790792369962, 0.0007283374762158154, -0.00032084656581056047, 0.0001646041074676944, -3.0003310202122244, 0.0006047324370085148, -0.00042555013540153966, -0.0006396618232154825, 4.988660226798091, -6.1443615346557e-05, 2.4948909560523433, 6.478701619722714e-05, -0.0006480203964514419, 3.2928787356556644, -0.00011788627795246733, 1.6669849337476699, 7.960341750094052e-05, 0.0010048939352689755, 0.0014223007560808846, -2.9993493557197306, 0.0001873509988254814, -0.0006047079448731891, -0.0005231007977422194]
+len_dofs = len(jnp.ravel(stel.dofs))
+dofs = jnp.reshape(jnp.array(x)[:len_dofs], shape=stel.dofs.shape)
+stel.dofs = dofs
+if len(x)>len_dofs:
+    print("Setting currents")
+    currents = jnp.array(x)[len_dofs:]
+    stel.currents = currents
+
+x0, y0, z0, vpar0, vperp0 = stel.initial_conditions(particles, R, r_init, model='Guiding Center')
+v0 = jnp.zeros((3, nparticles))
+normB0 = jnp.zeros((nparticles,))
+for i in range(nparticles):
+    B0 = B(jnp.array([x0[i],y0[i],z0[i]]), curve_segments=stel.gamma(n_segments), currents=stel.currents)
+    b0 = B0/jnp.linalg.norm(B0)
+    perp_vector_1 = jnp.array([0, b0[2], -b0[1]])
+    perp_vector_1_normalized = perp_vector_1/jnp.linalg.norm(perp_vector_1)
+    perp_vector_2 = jnp.cross(b0, perp_vector_1_normalized)
+    v0 = v0.at[:,i].set(vpar0[i]*b0 + vperp0[i]*(perp_vector_1_normalized/jnp.sqrt(2)+perp_vector_2/jnp.sqrt(2)))
+    normB0 = normB0.at[i].set(B_norm(jnp.array([x0[i],y0[i],z0[i]]), stel.gamma(n_segments), stel.currents))
+μ = particles.mass*vperp0**2/(2*normB0)
+
+time0 = time()
+trajectories_lorentz = stel.trace_trajectories_lorentz(particles, initial_values=jnp.array([x0, y0, z0, v0[0], v0[1], v0[2]]), maxtime=maxtime, timesteps=timesteps_lorentz, n_segments=n_segments)
+print(f"Time to trace trajectories Lorentz: {time()-time0:.2f} seconds")
+
+time0 = time()
+trajectories_guiding_center = stel.trace_trajectories(particles, initial_values=jnp.array([x0, y0, z0, vpar0, vperp0]), maxtime=maxtime, timesteps=timesteps_guiding_center, n_segments=n_segments)
+print(f"Time to trace trajectories Guiding Center: {time()-time0:.2f} seconds")
+
+# Define the plot setup function
+def setup_plots():
+    fig1 = plt.figure()
+    ax_3D = plt.axes(projection='3d')
+    ax_3D.set_xlabel("x [m]")
+    ax_3D.set_ylabel("y [m]")
+    ax_3D.set_zlabel("z [m]")
+    
+    fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+    
+    axs[0, 0].set_xlabel("time [s]")
+    axs[0, 0].set_ylabel("relative error in energy")
+    
+    axs[0, 1].set_xlabel("time [s]")
+    axs[0, 1].set_ylabel("v_par/v_th [m/s]")
+    
+    axs[1, 0].set_xlabel("R [m]")
+    axs[1, 0].set_ylabel("Z [m]")
+    
+    axs[1, 1].set_xlabel("time [s]")
+    axs[1, 1].set_ylabel("|B| [T]")
+    
+    return fig1, ax_3D, fig, axs
+
+# Plot trajectories
+def plot_trajectories(ax_3D, axs, times_gc, times_L, trajectories_lorentz, trajectories_guiding_center, colors):
+    ax_E, ax_vpar, ax_rz, ax_B = axs[0, 0], axs[0, 1], axs[1, 0], axs[1, 1]
+    
+    N_points_L = len(times_L)
+    N_points_gc = len(times_gc)
+    step_L = int(jnp.ceil(N_points_L / n_points_plot_L))
+    step_gc = int(jnp.ceil(N_points_gc / n_points_plot_gc))
+    for i, color in enumerate(colors):
+        # if i==0: continue
+        x_L, y_L, z_L, vx_L, vy_L, vz_L = trajectories_lorentz[i, ::step_L, :].transpose()
+        x_gc, y_gc, z_gc, vpar_gc = trajectories_guiding_center[i, ::step_gc, :].transpose()
+        
+        x_L = jnp.where(jnp.abs(x_L) > 1.5*(R+r), jnp.nan, x_L)
+        y_L = jnp.where(jnp.abs(y_L) > 1.5*(R+r), jnp.nan, y_L)
+        z_L = jnp.where(jnp.abs(z_L) > 1.5*r, jnp.nan, z_L)
+        
+        x_gc = jnp.where(jnp.abs(x_gc) > 1.5*(R+r), jnp.nan, x_gc)
+        y_gc = jnp.where(jnp.abs(y_gc) > 1.5*(R+r), jnp.nan, y_gc)
+        z_gc = jnp.where(jnp.abs(z_gc) > 1.5*r, jnp.nan, z_gc)
+
+        # ax_3D.plot(x_L, y_L, z_L, color=color)
+        ax_3D.plot(x_gc, y_gc, z_gc, color=color)
+        
+        r_gc = jnp.sqrt(x_gc**2 + y_gc**2)
+        r_L = jnp.sqrt(x_L**2 + y_L**2)
+        
+        ax_rz.plot(r_gc, z_gc, ':', color=color)
+        ax_rz.plot(r_L, z_L, '-', color=color)
+        
+        vth = jnp.sqrt(2*particles.energy/particles.mass)
+        v02_L = vx_L[0]**2 + vy_L[0]**2 + vz_L[0]**2
+        B_L = jnp.apply_along_axis(B, 1, jnp.array([x_L, y_L, z_L]).transpose(), stel.gamma(n_segments), stel.currents)
+        vpar_L = jnp.sum(jnp.array([vx_L, vy_L, vz_L])*B_L.transpose(), axis=0)/jnp.linalg.norm(B_L, axis=1)
+        normB_gc = jnp.apply_along_axis(B_norm, 1, jnp.array([x_gc, y_gc, z_gc]).transpose(), stel.gamma(n_segments), stel.currents)
+        normB_L = jnp.apply_along_axis(B_norm, 1, jnp.array([x_L, y_L, z_L]).transpose(), stel.gamma(n_segments), stel.currents)
+        
+        ax_vpar.plot(times_gc[::step_gc], vpar_gc/vth, ':', color=color)
+        ax_vpar.plot(times_L[::step_L], vpar_L/vth, '-', color=color)
+
+        ax_E.plot(times_gc[::step_gc], (jnp.array(μ[i]*normB_gc + 0.5*particles.mass*vpar_gc**2)-particles.energy)/particles.energy, ':', color=color)
+        ax_E.plot(times_L[::step_L], (vx_L**2 + vy_L**2 + vz_L**2 - v02_L) / v02_L, color=color)
+        
+        ax_B.plot(times_gc[::step_gc], normB_gc, ':', color=color)
+        ax_B.plot(times_L[::step_L], normB_L, '-', color=color)
+
+# Main plotting routine
+time0 = time()
+fig1, ax_3D, fig, axs = setup_plots()
+colors = cm.viridis(jnp.linspace(0, 1, nparticles))
+
+plot_trajectories(ax_3D, axs, jnp.linspace(0, maxtime, timesteps_guiding_center), jnp.linspace(0, maxtime, timesteps_lorentz), trajectories_lorentz, trajectories_guiding_center, colors)
+
+gamma = stel.gamma(n_segments)
+for i in range(n_curves * 2 * curves._nfp):
+    color = "orangered" if i < n_curves else "lightgrey"
+    ax_3D.plot(gamma[i, :, 0], gamma[i, :, 1], gamma[i, :, 2], color=color, zorder=10, linewidth=2)
+
+set_axes_equal(ax_3D)
+ax_3D.view_init(elev=20., azim=30)
+ax_3D.set_box_aspect([1,1,1], zoom=2)
+remove_3D_axes(ax_3D)
+# plt.tight_layout()
+
+print(f"Plotting trajectories: {time()-time0:.2f} seconds")
+
+plt.savefig("images/compare_lorentz_gc.pdf", bbox_inches='tight', pad_inches=0.1, dpi=300, transparent=True)
+plt.show()
--- examples_drift_field_coils_time/optimize_gc.py
+++ examples_drift_field_coils_time/optimize_gc.py
@@ -16,18 +16,18 @@ from simsopt.field import particles_to_vtk
 from pyevtk.hl import polyLinesToVTK
 import numpy as np
 
-n_particles=100
+n_particles=1000
 
-n_curves=2
+n_curves=4
 order=2
 nfp = 2
 
 A = 2.0 # Aspect ratio
 R = 7.75 # Major Radius
 r = R/A
-r_init = 0.05#r/5
+r_init = 0.3#r/5
 n_total_optimizations = 4
-axis_rc_zs = np.array([[1, 0.05], [0, 0.05]])*R
+axis_rc_zs = np.array([[1, 0.0], [0, 0.0]])*R#np.array([[1, 0.05], [0, 0.05]])*R
 optimize_with_respect_to_axis = True
 energy = 3.52e6 # eV
 B_on_axis = 5.7 # Tesla
@@ -42,9 +42,9 @@ optimize_least_squares = True
 # n_iteration_least_squares = [20]*1
 # diff_step_least_squares =   [None]*1
 # jax_grad_least_squares =    [True]*1
-n_iteration_least_squares = [20]*4
-diff_step_least_squares =   [1e-1, 1e-2, 1e-3, 1e-4]*1
-jax_grad_least_squares =    [False]*4
+n_iteration_least_squares = [20]#*4
+diff_step_least_squares =   [1e-1]#, 1e-2, 1e-3, 1e-4]*1
+jax_grad_least_squares =    [False]#*4
 # n_iteration_least_squares = [30]*7 #[150] + [50]*5 + [150]
 # diff_step_least_squares =   [None, 1e-2,  1e-3,  1e-4,  1e-5, 1e-6,  None]
 # jax_grad_least_squares =    [True, False, False, False, False, False, True]
@@ -54,7 +54,7 @@ ftol_least_squares = 1e-7
 
 model = "Guiding Center"
 
-advance_factor_each_optimization = 1.15
+advance_factor_each_optimization = 2.#1.15
 
 n_segments = order*25#100#int(max(50,order*10))
 
@@ -74,7 +74,7 @@ stel = Coils(curves, jnp.array([5.6*B_on_axis/len(curves._curves)]*n_curves))
 # stel.dofs = stel.dofs*(1+jax.random.normal(key, stel.dofs.shape)*1e-1)
 print(f"Dofs shape: {stel.dofs.shape}")
 
-initial_values = stel.initial_conditions(particles, R, r_init, model=model, more_trapped_particles=True, trapped_fraction_more=0.5, axis_rc_zs=axis_rc_zs if optimize_with_respect_to_axis else None, nfp=nfp)
+initial_values = stel.initial_conditions(particles, R, r_init, model=model, more_trapped_particles=False, trapped_fraction_more=0.5, axis_rc_zs=axis_rc_zs if optimize_with_respect_to_axis else None, nfp=nfp)
 initial_vperp = initial_values[4, :]
 
 time0 = time()
@@ -134,7 +134,7 @@ def create_field_lines(stel, maxtime, timesteps, n_segments, filename):
         ppl = np.asarray([xyz.shape[0] for xyz in res_tys])
         data = np.concatenate([i*np.ones((res_tys[i].shape[0], )) for i in range(len(res_tys))])
         polyLinesToVTK(filename, x, y, z, pointsPerLine=ppl, pointData={'idx': data})
-    # r_init = r/3
+    r_init = 0.5#r/3
     n_fieldlines = len(trajectories)
     angle = 0
     r_ = jnp.linspace(start=-r_init, stop=r_init, num=n_fieldlines)
@@ -142,7 +142,7 @@ def create_field_lines(stel, maxtime, timesteps, n_segments, filename):
     x_fl = (r_+R)*jnp.cos(phi)
     y_fl = (r_+R)*jnp.sin(phi)
     z_fl = jnp.zeros(n_fieldlines)
-    trajectories_fieldlines = stel.trace_fieldlines(jnp.array([x_fl, y_fl, z_fl]), maxtime/30, timesteps, n_segments)
+    trajectories_fieldlines = stel.trace_fieldlines(jnp.array([x_fl, y_fl, z_fl]), maxtime/50, timesteps, n_segments)
     particles_to_vtk_fl(res_tys=trajectories_fieldlines, filename=filename)
 
 def create_simsopt_curves(curves):
