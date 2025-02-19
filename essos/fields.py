60be0a0fe913a52407b7dfd0aafbce74ef5c58bb
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2025-02-19 11:15:51 -0600

    VMEC fields now working

--- essos/fields.py
+++ essos/fields.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from jax import jit, jacfwd
+from jax import jit, jacfwd, grad, vmap
 from functools import partial
 
 class BiotSavart():
@@ -23,4 +23,107 @@ class BiotSavart():
     @partial(jit, static_argnames=['self'])
     def dB_by_dX(self, points):
         return jacfwd(self.B)(points)
+    
+    @partial(jit, static_argnames=['self'])
+    def dAbsB_by_dX(self, points):
+        return grad(self.AbsB)(points)
 
+class Vmec():
+    def __init__(self, wout):
+        self.wout = wout
+        from netCDF4 import Dataset
+        self.nc = Dataset(self.wout)
+        self.nfp = self.nc.variables["nfp"][0]
+        self.bmnc = jnp.array(self.nc.variables["bmnc"][:])
+        self.xm = jnp.array(self.nc.variables["xm"][:])
+        self.xn = jnp.array(self.nc.variables["xn"][:])
+        self.rmnc = jnp.array(self.nc.variables["rmnc"][:])
+        self.zmns = jnp.array(self.nc.variables["zmns"][:])
+        self.bsubsmns = jnp.array(self.nc.variables["bsubsmns"][:])
+        self.bsubumnc = jnp.array(self.nc.variables["bsubumnc"][:])
+        self.bsubvmnc = jnp.array(self.nc.variables["bsubvmnc"][:])
+        self.gmnc = jnp.array(self.nc.variables["gmnc"][:])
+        self.xm_nyq = jnp.array(self.nc.variables["xm_nyq"][:])
+        self.xn_nyq = jnp.array(self.nc.variables["xn_nyq"][:])
+        self.len_xm_nyq = len(self.xm_nyq)
+        self.ns = self.nc.variables["ns"][0]
+        self.s_full_grid = jnp.linspace(0, 1, self.ns)
+        self.ds = self.s_full_grid[1] - self.s_full_grid[0]
+        self.s_half_grid = self.s_full_grid[1:] - 0.5 * self.ds
+        
+    @partial(jit, static_argnames=['self'])
+    def B(self, points):
+        s, theta, phi = points
+        bsubsmns_interp = vmap(lambda row: jnp.interp(s, self.s_half_grid, row), in_axes=1)(self.bsubsmns[1:, :])
+        bsubumnc_interp = vmap(lambda row: jnp.interp(s, self.s_half_grid, row), in_axes=1)(self.bsubumnc[1:, :])
+        bsubvmnc_interp = vmap(lambda row: jnp.interp(s, self.s_half_grid, row), in_axes=1)(self.bsubvmnc[1:, :])
+        gmnc_interp = vmap(lambda row: jnp.interp(s, self.s_half_grid, row), in_axes=1)(self.gmnc[1:])
+        rmnc_interp = vmap(lambda row: jnp.interp(s, self.s_full_grid, row), in_axes=1)(self.rmnc)
+        zmns_interp = vmap(lambda row: jnp.interp(s, self.s_full_grid, row), in_axes=1)(self.zmns)
+        d_rmnc_d_s_interp = vmap(lambda row: grad(lambda s: jnp.interp(s, self.s_full_grid, row))(s), in_axes=1)(self.rmnc)
+        d_zmns_d_s_interp = vmap(lambda row: grad(lambda s: jnp.interp(s, self.s_full_grid, row))(s), in_axes=1)(self.zmns)
+        
+        cosangle_nyq = jnp.cos(self.xm_nyq * theta - self.xn_nyq * phi)
+        sinangle_nyq = jnp.sin(self.xm_nyq * theta - self.xn_nyq * phi)
+        B_sub_s = jnp.dot(bsubsmns_interp, sinangle_nyq)
+        B_sub_theta = jnp.dot(bsubumnc_interp, cosangle_nyq)
+        B_sub_phi = jnp.dot(bsubvmnc_interp, cosangle_nyq)
+        sqrt_g_vmec = jnp.dot(gmnc_interp, cosangle_nyq)
+        
+        cosangle  = jnp.cos(self.xm * theta - self.xn * phi)
+        sinangle  = jnp.sin(self.xm * theta - self.xn * phi)
+        msinangle = self.xm * sinangle
+        nsinangle = self.xn * sinangle
+        mcosangle = self.xm * cosangle
+        ncosangle = self.xn * cosangle
+        
+        sinphi = jnp.sin(phi)
+        cosphi = jnp.cos(phi)
+        
+        R = jnp.dot(rmnc_interp, cosangle)
+        d_R_d_theta = jnp.dot(rmnc_interp, -msinangle)
+        d_R_d_phi   = jnp.dot(rmnc_interp, nsinangle)
+        d_R_d_s     = jnp.dot(d_rmnc_d_s_interp, cosangle)
+        
+        d_X_d_theta = d_R_d_theta * cosphi
+        d_X_d_phi = d_R_d_phi * cosphi - R * sinphi
+        d_X_d_s = d_R_d_s * cosphi
+
+        d_Y_d_theta = d_R_d_theta * sinphi
+        d_Y_d_phi = d_R_d_phi * sinphi + R * cosphi
+        d_Y_d_s = d_R_d_s * sinphi
+        
+        d_Z_d_s = jnp.dot(d_zmns_d_s_interp, sinangle)
+        d_Z_d_theta = jnp.dot(zmns_interp, mcosangle)
+        d_Z_d_phi = jnp.dot(zmns_interp, -ncosangle)
+
+        grad_s_X = (d_Y_d_theta * d_Z_d_phi - d_Z_d_theta * d_Y_d_phi) / sqrt_g_vmec
+        grad_s_Y = (d_Z_d_theta * d_X_d_phi - d_X_d_theta * d_Z_d_phi) / sqrt_g_vmec
+        grad_s_Z = (d_X_d_theta * d_Y_d_phi - d_Y_d_theta * d_X_d_phi) / sqrt_g_vmec
+
+        grad_theta_X = (d_Y_d_phi * d_Z_d_s - d_Z_d_phi * d_Y_d_s) / sqrt_g_vmec
+        grad_theta_Y = (d_Z_d_phi * d_X_d_s - d_X_d_phi * d_Z_d_s) / sqrt_g_vmec
+        grad_theta_Z = (d_X_d_phi * d_Y_d_s - d_Y_d_phi * d_X_d_s) / sqrt_g_vmec
+
+        grad_phi_X = (d_Y_d_s * d_Z_d_theta - d_Z_d_s * d_Y_d_theta) / sqrt_g_vmec
+        grad_phi_Y = (d_Z_d_s * d_X_d_theta - d_X_d_s * d_Z_d_theta) / sqrt_g_vmec
+        grad_phi_Z = (d_X_d_s * d_Y_d_theta - d_Y_d_s * d_X_d_theta) / sqrt_g_vmec
+        
+        return jnp.array([B_sub_s * grad_s_X + B_sub_theta * grad_theta_X + B_sub_phi * grad_phi_X,
+                          B_sub_s * grad_s_Y + B_sub_theta * grad_theta_Y + B_sub_phi * grad_phi_Y,
+                          B_sub_s * grad_s_Z + B_sub_theta * grad_theta_Z + B_sub_phi * grad_phi_Z])
+        
+    @partial(jit, static_argnames=['self'])
+    def AbsB(self, points):
+        s, theta, phi = points
+        bmnc_interp = vmap(lambda row: jnp.interp(s, self.s_half_grid, row), in_axes=1)(self.bmnc[1:, :])
+        cos_values = jnp.cos(self.xm_nyq * theta - self.xn_nyq * phi)
+        return jnp.dot(bmnc_interp, cos_values)
+    
+    @partial(jit, static_argnames=['self'])
+    def dB_by_dX(self, points):
+        return jacfwd(self.B)(points)
+    
+    @partial(jit, static_argnames=['self'])
+    def dAbsB_by_dX(self, points):
+        return grad(self.AbsB)(points)
---
+++ examples/vmec_SIMSOPT_vs_ESSOS.py
@@ -0,0 +1,70 @@
+from essos.fields import Vmec as Vmec_essos
+from simsopt.mhd import Vmec as Vmec_simsopt, vmec_compute_geometry
+from time import time
+import jax.numpy as jnp
+from jax import block_until_ready, random
+
+wout = "wout_LandremanPaul2021_QA_reactorScale_lowres_reference.nc"
+
+vmec_essos = Vmec_essos(wout)
+
+vmec_simsopt = Vmec_simsopt(wout)
+
+s_array=jnp.linspace(0.1, 0.99, 10)
+key = random.key(42)
+
+def absB_simsopt_func(s, theta, phi):
+    return vmec_compute_geometry(vmec_simsopt, s, theta, phi).modB[0][0][0]
+def absB_essos_func(s, theta, phi):
+    return vmec_essos.AbsB([s, theta, phi])
+def B_simsopt_func(s, theta, phi):
+    g = vmec_compute_geometry(vmec_simsopt, s, theta, phi)
+    return jnp.array([g.B_sub_s * g.grad_s_X + g.B_sub_theta_vmec * g.grad_theta_vmec_X + g.B_sub_phi * g.grad_phi_X,
+                      g.B_sub_s * g.grad_s_Y + g.B_sub_theta_vmec * g.grad_theta_vmec_Y + g.B_sub_phi * g.grad_phi_Y,
+                      g.B_sub_s * g.grad_s_Z + g.B_sub_theta_vmec * g.grad_theta_vmec_Z + g.B_sub_phi * g.grad_phi_Z])[:,0,0,0]
+def B_essos_func(s, theta, phi):
+    return vmec_essos.B([s, theta, phi])
+
+def timed_B(s, function):
+    theta = random.uniform(key=key, minval=0, maxval=2 * jnp.pi)
+    phi = random.uniform(key=key, minval=0, maxval=2 * jnp.pi)
+    function(s, theta, phi)
+    time1 = time()
+    B = block_until_ready(function(s, theta, phi))
+    time_taken = time()-time1
+    return time_taken, B
+
+average_time_modB_simsopt = 0
+average_time_modB_essos = 0
+average_time_B_essos = 0
+average_time_B_simsopt = 0
+error_modB = 0
+error_B = 0
+for s in s_array:
+    time_modB_simsopt, modB_simsopt = timed_B(s, absB_simsopt_func)
+    average_time_modB_simsopt += time_modB_simsopt
+    
+    time_modB_essos, modB_essos = timed_B(s, absB_essos_func)
+    average_time_modB_essos += time_modB_essos
+    
+    time_B_essos, B_essos = timed_B(s, B_essos_func)
+    average_time_B_essos += time_B_essos
+    
+    time_B_simsopt, B_simsopt = timed_B(s, B_simsopt_func)
+    average_time_B_simsopt += time_B_simsopt
+    
+    error_modB += jnp.abs((modB_simsopt-modB_essos)/modB_simsopt)
+    error_B += jnp.abs((B_simsopt-B_essos)/B_simsopt)
+
+average_time_modB_simsopt /= len(s_array)
+average_time_modB_essos /= len(s_array)
+average_time_B_essos /= len(s_array)
+average_time_B_simsopt /= len(s_array)
+print(f"Average time for modB simsopt: {average_time_modB_simsopt}")
+print(f"Average time for modB essos: {average_time_modB_essos}")
+print(f"Average time for B simsopt: {average_time_B_simsopt}")
+print(f"Average time for B essos: {average_time_B_essos}")
+print(f"Average error in modB: {error_modB*100}%")
+print(f"Max error in modB: {jnp.max(error_modB)*100}%")
+print(f"Average error in B: {error_B*100}%")
+print(f"Max error in B: {jnp.max(error_B)*100}%")
