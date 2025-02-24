81aafcae9736accd245f0daca969b76a35889496
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2025-02-24 11:12:40 -0600

    Refactor dynamics module to enhance trajectory tracing with new Tracing class and improve performance of guiding center and Lorentz functions

--- essos/dynamics.py
+++ essos/dynamics.py
@@ -1,139 +1,142 @@
 import jax.numpy as jnp
-from jax import jit, lax, random
+from jax import jit, lax, random, vmap
 from functools import partial
 from diffrax import diffeqsolve, ODETerm, SaveAt, Tsit5, PIDController
 
 q = 2*1.602176565e-19
 m = 4*1.660538921e-27
+c = 299792458
 
-@jit
-def GuidingCenter(t:              float,
-                  inital_values:  jnp.ndarray,
-                  args:           tuple) -> jnp.ndarray:
-    field, mu = args
+@partial(jit, static_argnums=(2))
+def GuidingCenter(t,
+                  initial_condition,
+                  field) -> jnp.ndarray:
 
-    assert isinstance(inital_values, jnp.ndarray), "initial values must be a jnp.ndarray"
-    assert inital_values.shape == (4,), "initial values must have shape (4,) with x, y, z, vpar"
-    assert inital_values.dtype == float, "initial values must be a float"
+    x, y, z, vpar = initial_condition
+    # condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
 
-    x, y, z, vpar = inital_values
-    condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
+    # def dxdt_dvdt(_):
+    points = jnp.array([x, y, z])
 
-    def dxdt_dvdt(_):
-        points = jnp.array([x, y, z])
+    B_covariant = field.B_covariant(points)
+    B_contravariant = field.B_contravariant(points)
+    AbsB = field.AbsB(points)
+    gradB = field.dAbsB_by_dX(points)
 
-        B_covariant = field.B_covariant(points)
-        B_contravariant = field.B_contravariant(points)
-        AbsB = field.AbsB(points)
-        gradB = field.dAbsB_by_dX(points)
+    omega = q*AbsB/m
 
-        omega = q*AbsB/m
+    dxdt = vpar*B_contravariant/AbsB + (vpar**2/omega+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB
+    dvdt = -mu/m*jnp.dot(B_contravariant,gradB)/AbsB
 
-        dxdt = vpar*B_contravariant/AbsB + (vpar**2/omega+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB
-        dvdt = -mu/m*jnp.dot(B_contravariant,gradB)/AbsB
+    return jnp.append(dxdt,dvdt)
 
-        return jnp.append(dxdt,dvdt)
+    # def zero_derivatives(_):
+    #     return jnp.zeros(4, dtype=float)
 
-    def zero_derivatives(_):
-        return jnp.zeros(4, dtype=float)
+    # return lax.cond(condition, zero_derivatives, dxdt_dvdt, operand=None)
 
-    return lax.cond(condition, zero_derivatives, dxdt_dvdt, operand=None)
+@partial(jit, static_argnums=(2))
+def Lorentz(t,
+            initial_condition,
+            field) -> jnp.ndarray:
+    
+    x, y, z, vx, vy, vz = initial_condition
+    # condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
 
-@jit
-def Lorentz(inital_values: jnp.ndarray,
-            t:             float,
-            field:          tuple) -> jnp.ndarray:
-    assert isinstance(inital_values, jnp.ndarray), "initial values must be a jnp.ndarray"
-    assert inital_values.shape == (6,), "initial values must have shape (6,) with x, y, z, vx, vy, vz"
-    assert inital_values.dtype == float, "initial values must be a float"
+    # def dxdt_dvdt(_):
+    points = jnp.array([x, y, z])
+    B_contravariant = field.B_contravariant(points)
 
-    x, y, z, vx, vy, vz = inital_values
-    condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
+    dxdt = jnp.array([vx, vy, vz])
+    dvdt = q / m * jnp.cross(dxdt, B_contravariant)
 
-    def dxdt_dvdt(_):
-        points = jnp.array([x, y, z])
-        B_contravariant = field.B_contravariant(points)
+    return jnp.append((dxdt, dvdt))
 
-        dxdt = jnp.array([vx, vy, vz])
-        dvdt = q / m * jnp.cross(dxdt, B_contravariant)
+    # def zero_derivatives(_):
+    #     return jnp.zeros(6, dtype=float)
 
-        return jnp.concatenate((dxdt, dvdt))
+    # return lax.cond(condition, zero_derivatives, dxdt_dvdt, operand=None)
 
-    def zero_derivatives(_):
-        return jnp.zeros(6, dtype=float)
-
-    return lax.cond(condition, zero_derivatives, dxdt_dvdt, operand=None)
-
-@jit
-def FieldLine(t:             float,
-              inital_values: jnp.ndarray,
+@partial(jit, static_argnums=(2))
+def FieldLine(t,
+              initial_condition,
               field) -> jnp.ndarray:
 
-    assert isinstance(inital_values, jnp.ndarray), "initial values must be a jnp.ndarray"
-    assert inital_values.shape == (3,), "initial values must have shape (3,) with x, y, z"
-    assert inital_values.dtype == float, "initial values must be a float"
+    # assert isinstance(initial_condition, jnp.ndarray), "initial values must be a jnp.ndarray"
+    # assert initial_condition.shape == (3,), "initial values must have shape (3,) with x, y, z"
+    # assert initial_condition.dtype == float, "initial values must be a float"
 
-    x, y, z = inital_values
-    key = random.PRNGKey(42)
-    random_value = random.choice(key, jnp.array([-1.0, 1.0]))
-    vpar = random_value*299792458 # speed of light
-    condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
+    x, y, z = initial_condition
+    velocity_signs = jnp.array([-1.0, 1.0])
+    plus1_minus1 = random.choice(random.PRNGKey(42), velocity_signs)
+    velocity = plus1_minus1*c # speed of light
+    # condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
 
-    def compute_derivatives(_):
-        position = jnp.array([x, y, z])
-        B_contravariant = field.B_contravariant(position)
-        AbsB = field.AbsB(position)
-        dxdt = vpar*B_contravariant/AbsB
-        return dxdt
+    # def compute_derivatives(_):
+    position = jnp.array([x, y, z])
+    B_contravariant = field.B_contravariant(position)
+    AbsB = field.AbsB(position)
+    dxdt = velocity*B_contravariant/AbsB
+    return dxdt
 
-    def zero_derivatives(_):
-        return jnp.zeros(3, dtype=float)
+    # def zero_derivatives(_):
+    #     return jnp.zeros(3, dtype=float)
 
-    return lax.cond(condition, zero_derivatives, compute_derivatives, operand=None)
+    # return lax.cond(condition, zero_derivatives, compute_derivatives, operand=None)
 
-@partial(jit, static_argnums=(1, 3, 4, 5, 6, 7,8))
-def trace_trajectories(
-    maxtime: float = 1e-7,
-    timesteps: int = 200,
-    initial_conditions: jnp.array = None
-):
-                    #    particles,
-                    #    field,
-                    #    initial_values: jnp.ndarray,
-                    #    maxtime: float = 1e-7,
-                    #    timesteps: int = 200,
-                    #    n_cores: int = len(devices()),
-                    #    adjoint=RecursiveCheckpointAdjoint(),
-                    #    tol_step_size = 5e-5,
-                    #    num_adaptative_steps=100000) -> jnp.ndarray:
+class Tracing():
+    def __init__(self, field, model):
+        self.field = field
+        self.model = model
+        
+        if model == 'GuidingCenter':
+            self.ODE_term = ODETerm(GuidingCenter)
+        elif model == 'Lorentz':
+            self.ODE_term = ODETerm(Lorentz)
+        elif model == 'FieldLine':
+            self.ODE_term = ODETerm(FieldLine)
 
-    times = jnp.linspace(0, maxtime, timesteps)
-    
-    
-    def compute_trajectory(particle_idx: jnp.ndarray,x_idx: jnp.ndarray,
-                                y_idx: jnp.ndarray,z_idx: jnp.ndarray,vpar_idx: jnp.ndarray,vperp_idx: jnp.ndarray) -> jnp.ndarray:
-        modB=field.norm_B(jnp.array((x_idx,y_idx,z_idx)))
-        mu = m * vperp_idx**2 / (2 * modB)
-        y0 = jnp.array((x_idx,y_idx,z_idx,vpar_idx))
-        args=(field, mu)
+    @partial(jit, static_argnums=(0, 2, 3))
+    def trace(self,
+        initial_conditions,
+        maxtime: float = 1e-7,
+        timesteps: int = 200,
+        tol_step_size = 5e-5,
+    ):
+                        #    particles,
+                        #    field,
+                        #    initial_values: jnp.ndarray,
+                        #    maxtime: float = 1e-7,
+                        #    timesteps: int = 200,
+                        #    n_cores: int = len(devices()),
+                        #    adjoint=RecursiveCheckpointAdjoint(),
+                        #    tol_step_size = 5e-5,
+                        #    num_adaptative_steps=100000) -> jnp.ndarray:
+        times = jnp.linspace(0, maxtime, timesteps)
+        
+        def compute_trajectory(initial_condition) -> jnp.ndarray:
+            # modB=field.norm_B(jnp.array((x_idx,y_idx,z_idx)))
+            # mu = m * vperp_idx**2 / (2 * modB)
+            # y0 = jnp.array((x_idx,y_idx,z_idx,vpar_idx))
+            # args=(field, mu)
 
-        trajectory = diffeqsolve(
-            ODETerm(GuidingCenter),
-            t0=0.0,
-            t1=maxtime,
-            dt0=maxtime / timesteps,
-            y0=y0,
-            solver=Tsit5(),
-            args=args,
-            saveat=SaveAt(ts=times),
-            throw=False,
-            adjoint=adjoint,
-            stepsize_controller = PIDController(pcoeff=0.3, icoeff=0.4, rtol=tol_step_size, atol=tol_step_size, dtmax=None,dtmin=None),
-            max_steps=num_adaptative_steps
-        ).ys
+            trajectory = diffeqsolve(
+                self.ODE_term,
+                t0=0.0,
+                t1=maxtime,
+                dt0=maxtime / timesteps,
+                y0=initial_condition,
+                solver=Tsit5(),
+                args=self.field,
+                saveat=SaveAt(ts=times),
+                throw=False,
+                # adjoint=adjoint,
+                stepsize_controller = PIDController(pcoeff=0.3, icoeff=0.4, rtol=tol_step_size, atol=tol_step_size, dtmax=None,dtmin=None),
+                # max_steps=num_adaptative_steps
+            ).ys
 
-        return trajectory
+            return trajectory
 
-    trajectories = vmap(aux_trajectory_device,in_axes=(0,0,0,0,0,0))(particles_indeces_part,x_part,y_part,z_part,vpar_part,vperp_part)
+        trajectories = vmap(compute_trajectory,in_axes=(0))(initial_conditions)
 
-    return trajectories
+        return trajectories
--- examples/gc_fl_SIMSOPT_vs_ESSOS.py
+++ examples/gc_fl_SIMSOPT_vs_ESSOS.py
@@ -1,34 +1,40 @@
 import os
-from essos.dynamics import GuidingCenter, Lorentz, FieldLine
-from simsopt.configs import get_ncsx_data
-from simsopt.field import BiotSavart, coils_via_symmetries
-import jax.numpy as jnp
-from essos.coils import Curves, Coils
-from simsopt import load
-from simsopt.field import (InterpolatedField, SurfaceClassifier, particles_to_vtk,
-                           compute_fieldlines, LevelsetStoppingCriterion, plot_poincare_data)
 import time
+import jax.numpy as jnp
+from simsopt import load
+from simsopt.field import (particles_to_vtk, compute_fieldlines, plot_poincare_data)
+from essos.coils import Coils_from_simsopt
+from essos.dynamics import Tracing
+from essos.fields import BiotSavart as BiotSavart_essos
 
-tmax_fl = 1000
-nfieldlines = 2
+tmax_fl = 600
+nfieldlines = 3
 axis_shft=0.02
 R0 = jnp.linspace(1.2125346+axis_shft, 1.295-axis_shft, nfieldlines)
 nfp = 2
 
 Z0 = jnp.zeros(nfieldlines)
-phis = [(i/4)*(2*jnp.pi/nfp) for i in range(4)]
+phi0 = jnp.zeros(nfieldlines)
+
+phis_poincare = [(i/4)*(2*jnp.pi/nfp) for i in range(4)]
 
 output_dir = os.path.join(os.path.dirname(__file__), 'output')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
     
 json_file = os.path.join(os.path.dirname(__file__), 'input', 'biot_savart_opt.json')
-bs = load(json_file)
+field_simsopt = load(json_file)
+field_essos = BiotSavart_essos(Coils_from_simsopt(json_file, nfp))
+
+tracing = Tracing(field=field_essos, model='FieldLine')
+fieldlines_essos = tracing.trace(initial_conditions=jnp.array([R0*jnp.cos(phi0), R0*jnp.sin(phi0), Z0]).T)
+print(fieldlines_essos)
+exit()
 
 t1 = time.time()
-fieldlines_tys, fieldlines_phi_hits = compute_fieldlines(bs, R0, Z0, tmax=tmax_fl, tol=1e-11, phis=phis)
+fieldlines_tys, fieldlines_phi_hits = compute_fieldlines(field_simsopt, R0, Z0, tmax=tmax_fl, tol=1e-11, phis=phis_poincare)
 t2 = time.time()
 print(f"Time for fieldline tracing={t2-t1:.3f}s. Num steps={sum([len(l) for l in fieldlines_tys])//nfieldlines}")
 
 particles_to_vtk(fieldlines_tys, os.path.join(output_dir,f'fieldlines_SIMSOPT'))
-plot_poincare_data(fieldlines_phi_hits, phis, os.path.join(output_dir,f'poincare_fieldline_SIMSOPT.pdf'), dpi=150)
+plot_poincare_data(fieldlines_phi_hits, phis_poincare, os.path.join(output_dir,f'poincare_fieldline_SIMSOPT.pdf'), dpi=150)
