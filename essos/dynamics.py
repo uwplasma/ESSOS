Fixed energy calculation for fo trajectories

essos/dynamics.py
@@ -190,7 +190,7 @@ class Tracing():
 
         assert timesteps is None or \
                isinstance(timesteps, (int, float)) and \
-               timesteps > 0, "timesteps must be None or a positive float"
+               timesteps > 0, f"timesteps must be None or a positive float. Got: {type(timesteps)}"
         assert times is None or \
                isinstance(times, jnp.ndarray), "times must be None or a numpy array"
         self.times = jnp.linspace(0, maxtime, timesteps) if times is None else times
@@ -313,7 +313,7 @@ class Tracing():
     def trajectories(self, value):
         self._trajectories = value
     
-    def _energy(self):
+    def energy(self):
         assert self.model in ['GuidingCenter', 'FullOrbit'], "Energy calculation is only available for GuidingCenter and FullOrbit models"
         mass = self.particles.mass
 
@@ -333,16 +333,12 @@ class Tracing():
         elif self.model == 'FullOrbit':
             def compute_energy(trajectory):
                 vxvyvz = trajectory[:, 3:]
-                v_squared = jnp.dot(vxvyvz, vxvyvz, axis=1)
+                v_squared = jnp.sum(jnp.square(vxvyvz), axis=1)
                 return 0.5 * mass * v_squared
             
             energy = vmap(compute_energy)(self.trajectories)
 
         return energy
-    
-    @property
-    def energy(self):
-        return self._energy()
     
     def to_vtk(self, filename):
         try: import numpy as np

examples/comparisons_SIMSOPT/fullorbit_SIMSOPT_vs_ESSOS.py
@@ -95,7 +95,7 @@ for j in range(nparticles):
 for i, SIMSOPT_energy_interp in enumerate(SIMSOPT_energy_interp_this_particle):
     plt.plot(trajectories_SIMSOPT_array[-1][-1][4:,0], jnp.mean(SIMSOPT_energy_interp, axis=0)[4:], '--', label=f'SIMSOPT Tol={trace_tolerance_SIMSOPT_array[i]}')
 for method_ESSOS, tracing, trajectories_ESSOS in zip(method_ESSOS_array, tracing_array, trajectories_ESSOS_array):
-    relative_energy_error_ESSOS = jnp.abs(tracing.energy-particles.energy)/particles.energy
+    relative_energy_error_ESSOS = jnp.abs(tracing.energy()-particles.energy)/particles.energy
     plt.plot(time_essos[2:], jnp.mean(relative_energy_error_ESSOS, axis=0)[2:], '-', label=f'ESSOS'+(' Boris' if method_ESSOS=='Boris' else f' Tol={trace_tolerance_ESSOS}'))
 plt.legend()
 plt.yscale('log')