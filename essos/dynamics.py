Adding Bstar correction. Fixing missing jacobian for flux coordinates in Guiding center. Changing form of guiding center RHS to better accomodate Bstar or any other additional terms like electric field, flows, anisotropies

essos/dynamics.py
@@ -146,6 +146,7 @@ def GuidingCenterCollisionsDriftMu(t,
     points = jnp.array([x, y, z])
     B_covariant = field.B_covariant(points)
     B_contravariant = field.B_contravariant(points)
+    sqrtg=field.sqrtg(points)     
     AbsB =field.AbsB(points)
     gradB = field.dAbsB_by_dX(points)
     AbsB_par=AbsB #should take into account B_par modification, but it does not matter for vacuum fields, so let's keep this for now
@@ -153,6 +154,9 @@ def GuidingCenterCollisionsDriftMu(t,
     v=jnp.sqrt(2./m*(0.5*m*vpar**2+mu*AbsB))
     xi=vpar/v
     p=m*v
+    Bstar=B_contravariant+vpar*m/q*field.curl_b(points)#+m/q*flow.curl_U0(points)
+    Ustar=vpar*B_contravariant/AbsB#+flow.U0(points) 
+    F_gc=mu*gradB+m*vpar**2*field.kappa(points)#-electric.electric(points)+vpar*flow.coriolis(points)+flow.centrifugal(points)        
     indeces_species=species.species_indeces
     nu_s=jnp.sum(jax.vmap(nu_s_ab,in_axes=(None,None,0,None,None,None))(m, q,indeces_species,v, points,species),axis=0)
     nu_D=jnp.sum(jax.vmap(nu_D_ab,in_axes=(None,None,0,None,None,None))(m, q,indeces_species,v, points,species),axis=0)
@@ -163,8 +167,10 @@ def GuidingCenterCollisionsDriftMu(t,
     d_Diffusion_par_dp=p*nu_par+p**2*dnu_par_dv/(2.*m)
     Avpar=-nu_s*vpar+vpar/p**2*(2.*(Diffusion_par-Diffusion_perp)+p*d_Diffusion_par_dp)
     Amu=-nu_s*2.*mu+2.*mu/p**2*(3.*(Diffusion_par-Diffusion_perp)+p*d_Diffusion_par_dp)+2.*Diffusion_perp/(m*AbsB)
-    dxdt = tag_gc*(vpar*B_contravariant/AbsB + (vpar**2/omega_mod+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB)
-    dvpardt=  Avpar-mu/m*jnp.dot(B_contravariant,gradB)/AbsB*tag_gc
+    dxdt =  tag_gc*(Ustar + jnp.cross(B_covariant, F_gc)/jnp.dot(B_covariant,Bstar)/q/sqrtg)
+    dvpardt = -jnp.dot(Bstar,F_gc)/jnp.dot(B_covariant,Bstar)*AbsB/m*tag_gc    
+    #dxdt = tag_gc*(vpar*B_contravariant/AbsB + (vpar**2/omega_mod+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB/sqrtg)
+    #dvpardt=  Avpar-mu/m*jnp.dot(B_contravariant,gradB)/AbsB*tag_gc
     dmudt = Amu
     return jnp.append(dxdt,jnp.append(dvpardt,dmudt))
 
@@ -182,7 +188,7 @@ def GuidingCenterCollisionsDiffusion(t,
     # condition = (jnp.sqrt(x**2 + y**2) > 10) | (jnp.abs(z) > 10)
     # def dxdt_dvdt(_):
     points = jnp.array([x, y, z])
-    B_contravariant = field.B_contravariant(points)
+    B_contravariant = field.B_contravariant(points)       
     AbsB = field.AbsB(points)
     #I_bb_tensor=jnp.identity(3)-jnp.diag(jnp.multiply(B_contravariant,B_contravariant))/AbsB**2
     I_bb_tensor=jnp.identity(3)-jnp.diag(jnp.multiply(B_contravariant,jnp.reshape(B_contravariant,(3,1))))/AbsB**2
@@ -221,9 +227,13 @@ def GuidingCenterCollisionsDrift(t,
     B_contravariant = field.B_contravariant(points)
     AbsB = field.AbsB(points)
     gradB = field.dAbsB_by_dX(points)
+    sqrtg=field.sqrtg(points)    
     mu = (m*v**2/2 - m*vpar**2/2)/AbsB
     omega = q*AbsB/m
     p=m*v
+    Bstar=B_contravariant+vpar*m/q*field.curl_b(points)#+m/q*flow.curl_U0(points)
+    Ustar=vpar*B_contravariant/AbsB#+flow.U0(points) 
+    F_gc=mu*gradB+m*vpar**2*field.kappa(points)#-electric.electric(points)+vpar*flow.coriolis(points)+flow.centrifugal(points)    
     indeces_species=species.species_indeces
     nu_s=jnp.sum(jax.vmap(nu_s_ab,in_axes=(None,None,0,None,None,None))(m, q,indeces_species,v, points,species),axis=0)
     nu_D=jnp.sum(jax.vmap(nu_D_ab,in_axes=(None,None,0,None,None,None))(m, q,indeces_species,v, points,species),axis=0)
@@ -232,9 +242,11 @@ def GuidingCenterCollisionsDrift(t,
     Diffusion_par=p**2/2.*nu_par
     Diffusion_perp=p**2/2.*nu_D 
     d_Diffusion_par_dp=p*nu_par+p**2/2.*dnu_par/m
-    dxdt = tag_gc*(vpar*B_contravariant/AbsB + (vpar**2/omega+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB)
+    dxdt =  tag_gc*(Ustar + jnp.cross(B_covariant, F_gc)/jnp.dot(B_covariant,Bstar)/q/sqrtg)
+    #dxdt = tag_gc*(vpar*B_contravariant/AbsB + (vpar**2/omega+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB/sqrtg)
     dvdt=(-nu_s*p+2.*Diffusion_par/p+d_Diffusion_par_dp*0.5)/m  #equation format was in p=m*v so we divide by m)
-    dxidt =-xi*2.*Diffusion_perp/p**2*0.5-mu/m*jnp.dot(B_contravariant,gradB)/AbsB/v*tag_gc
+    dxidt = -jnp.dot(Bstar,F_gc)/jnp.dot(B_covariant,Bstar)*AbsB/m/v*tag_gc
+    #dxidt =-xi*2.*Diffusion_perp/p**2*0.5-mu/m*jnp.dot(B_contravariant,gradB)/AbsB/v*tag_gc
     return jnp.append(dxdt,jnp.append(dvdt,dxidt))
 
 
@@ -257,8 +269,14 @@ def GuidingCenter(t,
     AbsB = field.AbsB(points)
     gradB = field.dAbsB_by_dX(points)
     mu = (E - m*vpar**2/2)/AbsB
+    sqrtg=field.sqrtg(points)
     omega = q*AbsB/m
-    dxdt = vpar*B_contravariant/AbsB + (vpar**2/omega+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB
+    #Bstar=B_contravariant+vpar*m/q*field.curl_b(points)#+m/q*flow.curl_U0(points)
+    #Ustar=vpar*B_contravariant/AbsB#+flow.U0(points) 
+    #F_gc=mu*gradB+m*vpar**2*field.kappa(points)#-electric.electric(points)+vpar*flow.coriolis(points)+flow.centrifugal(points)
+    #dxdt =  Ustar + jnp.cross(B_covariant, F_gc)/jnp.dot(B_covariant,Bstar)/q/sqrtg
+    #dvdt = -jnp.dot(Bstar,F_gc)/jnp.dot(B_covariant,Bstar)*AbsB/m    
+    dxdt = vpar*B_contravariant/AbsB + (vpar**2/omega+mu/q)*jnp.cross(B_covariant, gradB)/AbsB/AbsB/sqrtg
     dvdt = -mu/m*jnp.dot(B_contravariant,gradB)/AbsB
     return jnp.append(dxdt,dvdt)
     # def zero_derivatives(_):

essos/fields.py
@@ -14,6 +14,11 @@ class BiotSavart():
         self.gamma = coils.gamma
         self.gamma_dash = coils.gamma_dash
     
+
+    @partial(jit, static_argnames=['self'])
+    def sqrtg(self, points):
+        return 1.
+    
     @partial(jit, static_argnames=['self'])
     def B(self, points):
         dif_R = (jnp.array(points)-self.gamma).T
@@ -37,9 +42,29 @@ class BiotSavart():
     def dB_by_dX(self, points):
         return jacfwd(self.B)(points)
     
+    
     @partial(jit, static_argnames=['self'])
     def dAbsB_by_dX(self, points):
         return grad(self.AbsB)(points)
+    
+    @partial(jit, static_argnames=['self'])
+    def grad_B_covariant(self, points):
+        return jacfwd(self.B_covariant)(points)    
+ 
+    @partial(jit, static_argnames=['self'])
+    def curl_B(self, points):
+        grad_B_cov=self.grad_B_covariant(points)
+        return jnp.array([grad_B_cov[2][1] -grad_B_cov[1][2],
+                          grad_B_cov[0][2] -grad_B_cov[2][0],
+                          grad_B_cov[1][0] -grad_B_cov[0][1]])/self.sqrtg(points)
+    
+    @partial(jit, static_argnames=['self'])
+    def curl_b(self, points):
+        return self.curl_B(points)/self.AbsB(points)+jnp.cross(self.B_covariant(points),jnp.array(self.dAbsB_by_dX(points)))/self.AbsB(points)**2/self.sqrtg(points)
+
+    @partial(jit, static_argnames=['self'])
+    def kappa(self, points):
+        return -jnp.cross(self.B_contravariant(points),self.curl_b(points))*self.sqrtg(points)/self.AbsB(points)
     
     @partial(jit, static_argnames=['self'])
     def to_xyz(self, points):
@@ -102,7 +127,17 @@ class Vmec():
         B_sup_theta = jnp.dot(bsupumnc_interp, cosangle_nyq)
         B_sup_phi = jnp.dot(bsupvmnc_interp, cosangle_nyq)
         return jnp.array([0*B_sup_theta, B_sup_theta, B_sup_phi])
-    
+ 
+    @partial(jit, static_argnames=['self'])
+    def sqrtg(self, points):
+        s, theta, phi = points
+        gmnc_interp = vmap(lambda row: jnp.interp(s, self.s_half_grid, row, left='extrapolate'), in_axes=1)(self.gmnc[1:])
+        cosangle_nyq = jnp.cos(self.xm_nyq * theta - self.xn_nyq * phi)
+        sqrt_g_vmec = jnp.dot(gmnc_interp, cosangle_nyq)
+        return sqrt_g_vmec
+
+
+
     @partial(jit, static_argnames=['self'])
     def B(self, points):
         s, theta, phi = points
@@ -169,11 +204,33 @@ class Vmec():
     @partial(jit, static_argnames=['self'])
     def dB_by_dX(self, points):
         return jacfwd(self.B)(points)
+
+
     
     @partial(jit, static_argnames=['self'])
     def dAbsB_by_dX(self, points):
         return grad(self.AbsB)(points)
     
+    @partial(jit, static_argnames=['self'])
+    def grad_B_covariant(self, points):
+        return jacfwd(self.B_covariant)(points)    
+ 
+    @partial(jit, static_argnames=['self'])
+    def curl_B(self, points):
+        grad_B_cov=self.grad_B_covariant(points)
+        return jnp.array([grad_B_cov[2][1] -grad_B_cov[1][2],
+                          grad_B_cov[0][2] -grad_B_cov[2][0],
+                          grad_B_cov[1][0] -grad_B_cov[0][1]])/self.sqrtg(points)
+    
+    
+    @partial(jit, static_argnames=['self'])
+    def curl_b(self, points):
+        return self.curl_B(points)/self.AbsB(points)+jnp.cross(self.B_covariant(points),jnp.array(self.dAbsB_by_dX(points)))/self.AbsB(points)**2/self.sqrtg(points)
+
+    @partial(jit, static_argnames=['self'])
+    def kappa(self, points):
+        return -jnp.cross(self.B_contravariant(points),self.curl_b(points))*self.sqrtg(points)/self.AbsB(points)
+
     @partial(jit, static_argnames=['self'])
     def to_xyz(self, points):
         s, theta, phi = points

examples/trace_particles_coils_guidingcenter.py
@@ -1,33 +1,36 @@
 import os
-number_of_processors_to_use = 32 # Parallelization, this should divide nparticles
+number_of_processors_to_use = 8 # Parallelization, this should divide nparticles
 os.environ["XLA_FLAGS"] = f'--xla_force_host_platform_device_count={number_of_processors_to_use}'
 from time import time
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from essos.fields import BiotSavart
-from essos.coils import Coils_from_json
-from essos.constants import PROTON_MASS, ONE_EV
+from essos.coils import Coils_from_json,Coils_from_simsopt
+from essos.constants import ALPHA_PARTICLE_MASS, ALPHA_PARTICLE_CHARGE, FUSION_ALPHA_PARTICLE_ENERGY
 from essos.dynamics import Tracing, Particles
 
 # Input parameters
-tmax = 1e-4
-nparticles = number_of_processors_to_use*32
-R0 = jnp.linspace(1.23, 1.27, nparticles)
+tmax = 1e-7
+nparticles = number_of_processors_to_use*1
+R0 = 17.#jnp.linspace(1.23, 1.27, nparticles)
 trace_tolerance = 1e-7
-num_steps = 150000
-mass=PROTON_MASS
-energy=4000*ONE_EV
+num_steps = 5000
+energy=FUSION_ALPHA_PARTICLE_ENERGY#/10
 
 # Load coils and field
-json_file = os.path.join(os.path.dirname(__file__), 'input_files', 'ESSOS_biot_savart_LandremanPaulQA.json')
-coils = Coils_from_json(json_file)
+#json_file = os.path.join(os.path.dirname(__file__), 'input_files', 'ESSOS_biot_savart_LandremanPaulQA.json')
+#coils = Coils_from_json(json_file)
+#field = BiotSavart(coils)
+
+json_file = os.path.join(os.path.dirname(__file__), 'input_files', 'QI_nfp2.json')#'ESSOS_biot_savart_LandremanPaulQA.json')
+coils = Coils_from_simsopt(json_file,nfp=2)
 field = BiotSavart(coils)
 
 # Initialize particles
 Z0 = jnp.zeros(nparticles)
 phi0 = jnp.zeros(nparticles)
 initial_xyz=jnp.array([R0*jnp.cos(phi0), R0*jnp.sin(phi0), Z0]).T
-particles = Particles(initial_xyz=initial_xyz, mass=mass, energy=energy)
+particles = Particles(initial_xyz=initial_xyz, mass=ALPHA_PARTICLE_MASS,charge=ALPHA_PARTICLE_CHARGE, energy=energy)
 
 # Trace in ESSOS
 time0 = time()

examples/trace_particles_vmec_collisionsMu.py
@@ -5,18 +5,21 @@ from time import time
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from essos.fields import Vmec
-from essos.constants import ALPHA_PARTICLE_MASS, ALPHA_PARTICLE_CHARGE, FUSION_ALPHA_PARTICLE_ENERGY
+from essos.constants import ALPHA_PARTICLE_MASS, ALPHA_PARTICLE_CHARGE, FUSION_ALPHA_PARTICLE_ENERGY,PROTON_MASS,ELECTRON_MASS
 from essos.dynamics import Tracing, Particles
+from essos.background_species import BackgroundSpecies
 import numpy as np
 
+
 # Input parameters
-tmax = 1e-2
-nparticles = number_of_processors_to_use*10
+tmax = 1e-3
+dt=1.e-8
+nparticles = number_of_processors_to_use*1
 s = 0.25 # s-coordinate: flux surface label
 theta = jnp.linspace(0, 2*jnp.pi, nparticles)
 phi = jnp.linspace(0, 2*jnp.pi/2/2, nparticles)
-trace_tolerance = 1e-8
-num_steps_to_plot = 5000
+trace_tolerance = 1e-7
+num_steps_to_plot =  int(tmax/dt)
 energy=FUSION_ALPHA_PARTICLE_ENERGY#/10
 
 # Load coils and field
@@ -30,10 +33,27 @@ initial_xyz=jnp.array([[s]*nparticles, theta, phi]).T
 particles = Particles(initial_xyz=initial_xyz, mass=ALPHA_PARTICLE_MASS,
                       charge=ALPHA_PARTICLE_CHARGE, energy=energy, field=vmec)
 
+#Initialize background species
+#number_species=2  #(electrons,deuterium)
+#mass_array=jnp.array([ELECTRON_MASS/PROTON_MASS,2])    #mass_over_mproton
+number_species=2  #(electrons,deuterium)
+mass_array=jnp.array([ELECTRON_MASS/PROTON_MASS,2])    #mass_over_mproton
+charge_array=jnp.array([-1.,1.])    #mass_over_mproton
+T0=1.e+3  #eV
+n0=1e+20  #m^-3
+#n_array=[lambda x,y,z: n0,lambda x,y,z: n0 ]
+#T_array=[lambda x,y,z: T0,lambda x,y,z: T0 ]
+n_array=jnp.array([n0,n0])
+T_array=jnp.array([T0,T0])
+#n_array=jnp.array([n0, n0])
+#T_array=jnp.array([T0, T0])
+species = BackgroundSpecies(number_species=number_species, mass_array=mass_array, charge_array=charge_array, n_array=n_array, T_array=T_array)
+
+
 # Trace in ESSOS
 time0 = time()
-tracing = Tracing(field=vmec, model='GuidingCenter', particles=particles, maxtime=tmax,
-                  timesteps=num_steps_to_plot, tol_step_size=trace_tolerance)
+tracing = Tracing(field=vmec, model='GuidingCenterCollisionsMu', particles=particles, maxtime=tmax,
+                  timesteps=num_steps_to_plot, tol_step_size=trace_tolerance,species=species,tag_gc=1.)
 print(f"ESSOS tracing of {nparticles} particles during {tmax}s took {time()-time0:.2f} seconds")
 print(f"Final loss fraction: {tracing.loss_fractions[-1]*100:.2f}%")
 trajectories = tracing.trajectories
@@ -75,3 +95,37 @@ plt.savefig('vmec.png')
 # # Save results in vtk format to analyze in Paraview
 # vmec.surface.to_vtk('surface')
 # tracing.to_vtk('trajectories')
+
+
+# Plot trajectories, velocity parallel to the magnetic field, and energy error
+fig = plt.figure(figsize=(9, 8))
+ax1 = fig.add_subplot(221, projection='3d')
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
+
+vmec.surface.plot(ax=ax1, show=False, alpha=0.4)
+tracing.plot(ax=ax1, show=False, n_trajectories_plot=nparticles)
+
+v=jnp.sqrt(tracing.energy*2./particles.mass)
+
+for i, trajectory in enumerate(trajectories):
+    #ax2.plot(tracing.times, (tracing.energy[i]-tracing.energy[i,0])/tracing.energy[i,0], label=f'Particle {i+1}')
+    ax2.plot(tracing.times, (v[i]-v[i,0])/v[i,0], label=f'Particle {i+1}')    
+    ax3.plot(tracing.times, trajectory[:, 3]/jnp.sqrt(tracing.energy[i]/particles.mass*2.), label=f'Particle {i+1}')
+    ax4.plot(trajectory[:,0], label=f'Particle {i+1}')
+
+
+
+
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Normalized energy variation')
+ax3.set_ylabel(r'$v_{\parallel}/v$')
+#ax2.legend()
+ax3.set_xlabel('Time (s)')
+#ax3.legend()
+ax4.set_xlabel('R (m)')
+ax4.set_ylabel('Z (m)')
+#ax4.legend()
+plt.tight_layout()
+plt.savefig('traj_vmec.png')