62b86314ef76d57ee4dbd780398eff629e46ca47
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2025-08-13 22:59:47 +0100

    trace_fieldlines fixed for coils and vmec (XYZ conversion needed)

--- examples/trace_fieldlines_coils.py
+++ examples/trace_fieldlines_coils.py
@@ -1,20 +1,21 @@
 import os
-number_of_processors_to_use = 1 # Parallelization, this should divide nfieldlines
+number_of_processors_to_use = 6 # Parallelization, this should divide nfieldlines
 os.environ["XLA_FLAGS"] = f'--xla_force_host_platform_device_count={number_of_processors_to_use}'
 from time import time
 import jax.numpy as jnp
+from jax import block_until_ready
 import matplotlib.pyplot as plt
 from essos.fields import BiotSavart
 from essos.coils import Coils_from_json
 from essos.dynamics import Tracing
 
 # Input parameters
-tmax = 100.
-nfieldlines_per_core=12
+tmax = 1000
+nfieldlines_per_core=3
 nfieldlines = number_of_processors_to_use*nfieldlines_per_core
 R0 = jnp.linspace(1.21, 1.4, nfieldlines)
 trace_tolerance = 1e-8
-num_steps = 1000
+num_steps = 6000
 
 # Load coils and field
 json_file = os.path.join(os.path.dirname(__file__), 'input_files', 'ESSOS_biot_savart_LandremanPaulQA.json')
@@ -28,8 +29,8 @@ initial_xyz=jnp.array([R0*jnp.cos(phi0), R0*jnp.sin(phi0), Z0]).T
 
 # Trace in ESSOS
 time0 = time()
-tracing = Tracing(field=field, model='FieldLineAdaptative', initial_conditions=initial_xyz,
-                  maxtime=tmax, times_to_trace=num_steps, atol=trace_tolerance,rtol=trace_tolerance)
+tracing = block_until_ready(Tracing(field=field, model='FieldLineAdaptative', initial_conditions=initial_xyz,
+                  maxtime=tmax, times_to_trace=num_steps, atol=trace_tolerance,rtol=trace_tolerance))
 print(f"ESSOS tracing took {time()-time0:.2f} seconds")
 trajectories = tracing.trajectories
 
--- examples/trace_fieldlines_vmec.py
+++ examples/trace_fieldlines_vmec.py
@@ -1,20 +1,20 @@
 import os
-number_of_processors_to_use = 1 # Parallelization, this should divide nfieldlines
+number_of_processors_to_use = 3 # Parallelization, this should divide nfieldlines
 os.environ["XLA_FLAGS"] = f'--xla_force_host_platform_device_count={number_of_processors_to_use}'
 from time import time
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+from jax import block_until_ready, vmap
 from essos.fields import Vmec
-from essos.coils import Coils_from_json
 from essos.dynamics import Tracing
 
 # Input parameters
-tmax = 0.1
-nfieldlines_per_core=12
+tmax = 1500
+nfieldlines_per_core=2
 nfieldlines = number_of_processors_to_use*nfieldlines_per_core
-R0 = jnp.linspace(1.21, 1.4, nfieldlines)
-trace_tolerance = 1e-8
-num_steps = 1000
+R0 = jnp.linspace(0.05, 0.6, nfieldlines)
+trace_tolerance = 1e-10
+num_steps = 10000
 
 # Load coils and field
 wout_file = os.path.join(os.path.dirname(__file__), 'input_files',"wout_QH_simple_scaled.nc")
@@ -27,8 +27,8 @@ initial_xyz=jnp.array([R0*jnp.cos(phi0), R0*jnp.sin(phi0), Z0]).T
 
 # Trace in ESSOS
 time0 = time()
-tracing = Tracing(field=vmec, model='FieldLineAdaptative', initial_conditions=initial_xyz,
-                  maxtime=tmax, times_to_trace=num_steps, atol=trace_tolerance,rtol=trace_tolerance)
+tracing = block_until_ready(Tracing(field=vmec, model='FieldLineAdaptative', initial_conditions=initial_xyz,
+                  maxtime=tmax, times_to_trace=num_steps, atol=trace_tolerance,rtol=trace_tolerance))
 print(f"ESSOS tracing took {time()-time0:.2f} seconds")
 trajectories = tracing.trajectories
 
@@ -38,7 +38,11 @@ ax1 = fig.add_subplot(121, projection='3d')
 ax2 = fig.add_subplot(122)
 vmec.surface.plot(ax=ax1, show=False)
 tracing.plot(ax=ax1, show=False)
-tracing.poincare_plot(ax=ax2, show=False, shifts=[0, jnp.pi/2])#, jnp.pi/4, jnp.pi/2, 3*jnp.pi/4])
+
+# Convert s, theta, phi coordinates to XYZ
+tracing.trajectories = vmap(vmap(vmec.to_xyz, in_axes=0), in_axes=0)(trajectories)
+    
+tracing.poincare_plot(ax=ax2, show=False, shifts=[0])
 plt.tight_layout()
 plt.show()
 
