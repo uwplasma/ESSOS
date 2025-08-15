a605580c1660d4d98d13860bbc636f393c1f5f3b
eduardolneto <eduardo.lascasneto@gmail.com>
2025-08-15 20:38:27 +0100

    Adding requirements for optuna and pandas, at least for tests to run on github

--- tests/test_multiobjectives.py
+++ tests/test_multiobjectives.py
@@ -3,17 +3,27 @@ from unittest.mock import MagicMock, patch
 from essos.multiobjectiveoptimizer import MultiObjectiveOptimizer
 from essos.coils import Coils,Curves
 from essos.fields import BiotSavart
+from essos.objective_functions import loss_bdotn_over_b, loss_coil_length, loss_coil_curvature, loss_normB_axis
 
 # test_multiobjectiveoptimizer.py
 
 import jax.numpy as jnp
 
 
+
+
+def surface():
+    surface.nphi=3
+    surface.ntheta=3
+    surface.gamma = jnp.ones((3, 3, 3))
+    surface.unitnormal = jnp.ones((3, 3, 3))    
+    return surface
+
 def mock_vmec():
     vmec = MagicMock()
     vmec.nfp = 2
     vmec.r_axis = 10.0
-    vmec.surface = MagicMock()
+    vmec.surface = surface()
     return vmec
 
 
@@ -57,7 +67,20 @@ def test_build_available_inputs( vmec=mock_vmec(),  dummy_loss_fn=dummy_loss_fn(
     assert loss_weight_result.shape == ()
     assert loss_weight_result == 496
 
-    optimization_result=optimizer.optimize_with_optax(weights, method="adam", lr=1e-2)
-    assert optimization_result.currents_scale==0.01999998979999997872
-    
+    optimized_coils=optimizer.optimize_with_optax(weights, method="adam", lr=1e-2)
+    assert optimized_coils.currents_scale==0.01999998979999997872
+
+    dofs_curves=optimized_coils.dofs_curves
+    currents_scale=optimized_coils.currents_scale
+    nfp=optimized_coils.nfp
+    n_segments=optimized_coils.n_segments
+    stellsym=optimized_coils.stellsym
+    x=optimized_coils.x
+    bdotn_b=loss_bdotn_over_b(x,vmec=vmec,dofs_curves=dofs_curves, currents_scale=currents_scale, nfp=nfp, n_segments=n_segments, stellsym=stellsym)
+    assert bdotn_b==0.0000000000000037761977058799732810080238
+
+    max_length=loss_coil_length(x,dofs_curves=dofs_curves, currents_scale=currents_scale, nfp=nfp, n_segments=n_segments, stellsym=stellsym)
+    max_curvature=loss_coil_curvature(x,dofs_curves=dofs_curves, currents_scale=currents_scale, nfp=nfp, n_segments=n_segments, stellsym=stellsym)
+    normB_axis=loss_normB_axis(x,dofs_curves=dofs_curves, currents_scale=currents_scale, nfp=nfp, n_segments=n_segments, stellsym=stellsym)
+
     optimizer.run()
