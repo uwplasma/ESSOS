463a7a6d3eeffe9114299fd43329ed8874c49dfb
eduardolneto <eduardo.lascasneto@gmail.com>
2025-08-20 18:43:48 +0000

    modifieng test for biot_savart initialization to comply wih changes

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -8,6 +8,7 @@ class MockCoils:
         self.currents = jnp.array([1.0, 2.0, 3.0])
         self.gamma = random.uniform(random.PRNGKey(0), (3, 3, 3))
         self.gamma_dash = random.uniform(random.PRNGKey(0), (3, 3, 3))
+        self.gamma_dash = random.uniform(random.PRNGKey(0), (3, 3, 3))
         self.dofs_curves = random.uniform(random.PRNGKey(0), (3, 3, 3))
 
 def test_biot_savart_initialization():
