37252efe8a6e4a2670dfe1565939d72407109511
EstevaoMGomes <estevao.gomes04@gmail.com>
2025-10-29 00:09:43 +0100

    Fix(analysis): precompile coil gammas for comparison with simsopt

--- analysis/comparisons_simsopt/coils.py
+++ analysis/comparisons_simsopt/coils.py
@@ -80,6 +80,9 @@ for nfp, curves_stel, currents_stel, name in zip(nfp_array, curves_array, curren
     [curve.gammadash() for curve in curves_simsopt]
     [curve.gammadashdash() for curve in curves_simsopt]
     coils_essos.gamma
+    coils_essos.gamma_dash
+    coils_essos.gamma_dashdash
+    coils_essos.curvature
     coils_essos.reset_cache()
     
     # Running the second time for coils characteristics comparison
