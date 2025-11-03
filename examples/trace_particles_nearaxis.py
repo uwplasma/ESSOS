105f9132da291f05038bf58d24fb65e9007bd14a
eduardolneto <eduardo.lascasneto@gmail.com>
2025-11-03 04:42:16 +0000

    Added a near_axis_test class for tracing in fields.py. Changed definitions of B quantities to match r,theta_B,phi_B coordinates instead of psi,vartheta,phi_B. Added example trace_particles_nearxis.py

--- examples/trace_particles_nearaxis.py
+++ examples/trace_particles_nearaxis.py
@@ -4,41 +4,47 @@ os.environ["XLA_FLAGS"] = f'--xla_force_host_platform_device_count={number_of_pr
 from time import time
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
-from essos.fields import Vmec
-from essos.constants import ALPHA_PARTICLE_MASS, ALPHA_PARTICLE_CHARGE, FUSION_ALPHA_PARTICLE_ENERGY
+from essos.fields import near_axis_test
+from essos.constants import ALPHA_PARTICLE_MASS, ALPHA_PARTICLE_CHARGE, FUSION_ALPHA_PARTICLE_ENERGY,PROTON_MASS,ELEMENTARY_CHARGE
 from essos.dynamics import Tracing, Particles
 import numpy as np
 
 
 
+# Initialize Near-Axis field
+rc=jnp.array([1, 0.045])
+zs=jnp.array([0,-0.045])
+etabar=-0.9
+nfp=3
+field = near_axis_test(rc=rc, zs=zs, etabar=etabar, nfp=nfp)
+
+
 # Input parameters
-tmax = 1e-4
 timestep = 1.e-8
-times_to_trace=5000
-nparticles_per_core=6
+times_to_trace=1000
+nparticles_per_core=4
 nparticles = number_of_processors_to_use*nparticles_per_core
 n_particles_to_plot = 4
-s = 0.6 # s-coordinate: flux surface label
-theta = jnp.linspace(0, 2*jnp.pi, nparticles)
-phi = jnp.linspace(0, 2*jnp.pi/2/4, nparticles)
 atol = 1e-8
 rtol = 1e-8
 energy=FUSION_ALPHA_PARTICLE_ENERGY
 
-# Load coils and field
-wout_file = os.path.join(os.path.dirname(__file__), "input_files", "wout_LandremanPaul2021_QA_reactorScale_lowres.nc")
-vmec = Vmec(wout_file)
 
-# Initialize particles
+# Trace fieldlines
+tmax = 1.e-7
+
+R0 = jnp.linspace(field.R0[0], 1.05*field.R0[0], nparticles)
 Z0 = jnp.zeros(nparticles)
 phi0 = jnp.zeros(nparticles)
-initial_xyz=jnp.array([[s]*nparticles, theta, phi]).T
-particles = Particles(initial_xyz=initial_xyz, mass=ALPHA_PARTICLE_MASS,
-                      charge=ALPHA_PARTICLE_CHARGE, energy=energy, field=vmec)
+initial_xyz=jnp.array([R0*jnp.cos(phi0), R0*jnp.sin(phi0), Z0]).T
+
+
+#particles = Particles(initial_xyz=initial_xyz, mass=ALPHA_PARTICLE_MASS,charge=ALPHA_PARTICLE_CHARGE, energy=energy, field=vmec)
+particles = Particles(initial_xyz=initial_xyz, mass=2.*PROTON_MASS,charge=ELEMENTARY_CHARGE, energy=energy, field=field)
 
 # Trace in ESSOS
 time0 = time()
-tracing = Tracing(field=vmec, model='GuidingCenterAdaptative', particles=particles, maxtime=tmax,
+tracing = Tracing(field=field, model='GuidingCenterAdaptative', particles=particles, maxtime=tmax,
                   timestep=timestep,times_to_trace=times_to_trace, atol=atol,rtol=rtol)
 print(f"ESSOS tracing of {nparticles} particles during {tmax}s took {time()-time0:.2f} seconds")
 print(f"Final loss fraction: {tracing.loss_fractions[-1]*100:.2f}%")
@@ -53,7 +59,6 @@ ax4 = fig.add_subplot(224)
 
 # Plot 5 random particles
 ## Plot trajectories in 3D
-vmec.surface.plot(ax=ax1, show=False, alpha=0.4)
 tracing.plot(ax=ax1, show=False, n_trajectories_plot=nparticles)
 for i in np.random.choice(nparticles, size=n_particles_to_plot, replace=False):
     trajectory = trajectories[i]
@@ -76,7 +81,7 @@ ax3.set_ylabel(r'$v_{\parallel}/v$')
 ax3.set_xlabel('Time (s)')
 ax4.set_xlabel('Time (s)')
 plt.tight_layout()
-plt.show()
+plt.savefig('trace_particles_nearaxis.png', dpi=300)
 
 # # Save results in vtk format to analyze in Paraview
 # vmec.surface.to_vtk('surface')
