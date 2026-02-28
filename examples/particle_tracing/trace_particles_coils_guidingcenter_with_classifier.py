85f1f1325194bc384034a3df72fc68d8d3009f7c
EstevaoMGomes <estevao.gomes04@gmail.com>
2026-02-28 20:38:46 +0100

    [wip] trace_particles_coils_guidingcenter_with_classifier now runs but for some reason it gets stuck running

--- examples/particle_tracing/trace_particles_coils_guidingcenter_with_classifier.py
+++ examples/particle_tracing/trace_particles_coils_guidingcenter_with_classifier.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from essos.fields import BiotSavart,Vmec
 from essos.surfaces import SurfaceClassifier
-from essos.coils import Coils_from_simsopt
+from essos.coils import Coils
 from essos.constants import ALPHA_PARTICLE_MASS, ALPHA_PARTICLE_CHARGE, FUSION_ALPHA_PARTICLE_ENERGY,ONE_EV
 from essos.dynamics import Tracing, Particles
 
@@ -25,13 +25,13 @@ energy=FUSION_ALPHA_PARTICLE_ENERGY
 
 
 # Load coils and field
-json_file = os.path.join(os.path.dirname(__name__), 'input_files', 'QH_simple_scaled.json')
-coils = Coils_from_simsopt(json_file,nfp=4)
+json_file = os.path.join(os.path.dirname(__name__), '../input_files', 'QH_simple_scaled.json')
+coils = Coils.from_simsopt(json_file)
 field = BiotSavart(coils)
 
 
 # Load coils and field
-wout_file = os.path.join(os.path.dirname(__name__), 'input_files','wout_QH_simple_scaled.nc')
+wout_file = os.path.join(os.path.dirname(__name__), '../input_files','wout_QH_simple_scaled.nc')
 vmec = Vmec(wout_file)
 
 timeI=time()
