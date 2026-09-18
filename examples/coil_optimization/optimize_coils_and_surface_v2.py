113a0bb875fdb11258d4c4fd1132b5c5b0cce8aa
Gonzalo_ftd <gftd13@gmail.com>
2026-09-18 18:23:26 -0500

    Changed some variable names in the example

--- examples/coil_optimization/optimize_coils_and_surface_v2.py
+++ examples/coil_optimization/optimize_coils_and_surface_v2.py
@@ -63,16 +63,16 @@ COIL_CURRENT = 1.  # Amperes (optimization does not depend on current magnitude)
 # Initialize the coils and the field
 init_curves = CreateEquallySpacedCurves(N_COILS, FOURIER_ORDER, LARGE_R, SMALL_R, n_segments=N_SEGMENTS, nfp=NFP, stellsym=STELLSYM)
 init_coils = Coils(curves=init_curves, currents=[COIL_CURRENT]*N_COILS)
-init_field = BiotSavart(init_coils)
+field_init = BiotSavart(init_coils)
 
 # Initialize the surface from a VMEC output file.
-surface = SurfaceRZFourier.from_wout_file(vmec_input, s=1, ntheta=30, nphi=30, range_torus='half period')
+surface_init = SurfaceRZFourier.from_wout_file(vmec_input, s=1, ntheta=ntheta, nphi=nphi, range_torus='half period')
 
 # Build and cache surface geometry before JAX starts the optimization.
-surface.gamma.block_until_ready()
-surface.unitnormal.block_until_ready()
+surface_init.gamma.block_until_ready()
+surface_init.unitnormal.block_until_ready()
 
-CROSS_SECTIONAL_AREA_TARGET = float( surface.area_section_by_phi() )
+CROSS_SECTIONAL_AREA_TARGET = float( surface_init.area_section_by_phi() )
 
 # ====================================================================================
 # ====================================================================================
@@ -101,7 +101,7 @@ L_total = ( NORMAL_FIELD_WEIGHT*L_normal_field +
            QS_WEIGHT*L_quasi_symmetry )
 
 # The dependencies of the total loss are set to the field and surface. Both will be mdified during the optimization.
-L_total.dependencies = { "field": init_field, "surface": surface }
+L_total.dependencies = { "field": field_init, "surface": surface_init }
 
 
 # We assign different scaling factors to the field and surface dofs to balance their characteristic step sizes.
@@ -156,17 +156,17 @@ print("Loss after optimization:", L_total(res.x))
 opt_dict = L_total.dofs_to_pytree(res.x)
 
 # The optimized field and surface are extracted from the results of the optimization.
-opt_field = opt_dict["field"]
-opt_surface = opt_dict["surface"]
+field_opt = opt_dict["field"]
+surface_opt = opt_dict["surface"]
 
 # Coils geometry is extracted from the optimized field.
-opt_coils = opt_field.coils
+opt_coils = field_opt.coils
 
 
 # Surface displacement between corresponding grid points
-surface_displacement_xyz = opt_surface.gamma - surface.gamma
+surface_displacement_xyz = surface_opt.gamma - surface_init.gamma
 surface_displacement = jnp.linalg.norm(surface_displacement_xyz, axis=2)# Total displacement magnitude
-surface_normal_displacement = jnp.sum( surface_displacement_xyz * surface.unitnormal, axis=2 ) # Displacement perpendicular to the initial surface
+surface_normal_displacement = jnp.sum( surface_displacement_xyz * surface_init.unitnormal, axis=2 ) # Displacement perpendicular to the initial surface
 
 
 
@@ -188,58 +188,58 @@ print("Maximum absolute normal displacement:", jnp.max(jnp.abs(surface_normal_di
 
 print("\n---------------------------------------------------------------------------")
 print("\nNormal-field residuals and losses:")
-B_dot_n_over_B_init = BdotN_over_B( surface , init_field )
-B_dot_n_over_B_opt = BdotN_over_B( opt_surface , opt_field )
+B_dot_n_over_B_init = BdotN_over_B( surface_init , field_init )
+B_dot_n_over_B_opt = BdotN_over_B( surface_opt , field_opt )
 print("mean abs residual (initial):", jnp.mean(jnp.abs(B_dot_n_over_B_init)) )
 print("mean abs residual (optimized):",jnp.mean(jnp.abs(B_dot_n_over_B_opt)) )
 print("max abs residual (initial):",jnp.max(jnp.abs(B_dot_n_over_B_init)) )
 print("max abs residual (optimized):",jnp.max(jnp.abs(B_dot_n_over_B_opt)) )
-print("Normal-field loss (initial):",loss_BdotN_mean(init_field, surface) )
-print("Normal-field loss (optimized):",loss_BdotN_mean(opt_field, opt_surface) )
+print("Normal-field loss (initial):",loss_BdotN_mean(field_init, surface_init) )
+print("Normal-field loss (optimized):",loss_BdotN_mean(field_opt, surface_opt) )
 
 print("\n---------------------------------------------------------------------------")
 print("\nCoil-length residuals and losses:")
-coil_length_residual_init = jnp.maximum(0, init_field.coils.length - LENGTH_TARGET)
-coil_length_residual_opt = jnp.maximum(0, opt_field.coils.length - LENGTH_TARGET)
-print("Coil lengths (initial):", init_field.coils.length)
-print("Coil lengths (optimized):", opt_field.coils.length)
+coil_length_residual_init = jnp.maximum(0, field_init.coils.length - LENGTH_TARGET)
+coil_length_residual_opt = jnp.maximum(0, field_opt.coils.length - LENGTH_TARGET)
+print("Coil lengths (initial):", field_init.coils.length)
+print("Coil lengths (optimized):", field_opt.coils.length)
 print("mean excess length (initial):", jnp.mean(coil_length_residual_init))
 print("mean excess length (optimized):", jnp.mean(coil_length_residual_opt))
 print("max excess length (initial):", jnp.max(coil_length_residual_init))
 print("max excess length (optimized):", jnp.max(coil_length_residual_opt))
-print("Length loss (initial):", loss_coil_length_max(init_field, max_coil_length=LENGTH_TARGET))
-print("Length loss (optimized):", loss_coil_length_max(opt_field, max_coil_length=LENGTH_TARGET))
+print("Length loss (initial):", loss_coil_length_max(field_init, max_coil_length=LENGTH_TARGET))
+print("Length loss (optimized):", loss_coil_length_max(field_opt, max_coil_length=LENGTH_TARGET))
 
 print("\n---------------------------------------------------------------------------")
 print("\nCoil-curvature residuals and losses:")
-coil_curvature_residual_init = jnp.maximum(0, init_field.coils.curvature - CURVATURE_TARGET)
-coil_curvature_residual_opt = jnp.maximum(0, opt_field.coils.curvature - CURVATURE_TARGET)
-print("mean curvature (initial):", jnp.mean(init_field.coils.curvature))
-print("mean curvature (optimized):", jnp.mean(opt_field.coils.curvature))
-print("max curvature (initial):", jnp.max(init_field.coils.curvature))
-print("max curvature (optimized):", jnp.max(opt_field.coils.curvature))
+coil_curvature_residual_init = jnp.maximum(0, field_init.coils.curvature - CURVATURE_TARGET)
+coil_curvature_residual_opt = jnp.maximum(0, field_opt.coils.curvature - CURVATURE_TARGET)
+print("mean curvature (initial):", jnp.mean(field_init.coils.curvature))
+print("mean curvature (optimized):", jnp.mean(field_opt.coils.curvature))
+print("max curvature (initial):", jnp.max(field_init.coils.curvature))
+print("max curvature (optimized):", jnp.max(field_opt.coils.curvature))
 print("mean excess curvature (initial):", jnp.mean(coil_curvature_residual_init))
 print("mean excess curvature (optimized):", jnp.mean(coil_curvature_residual_opt))
 print("max excess curvature (initial):", jnp.max(coil_curvature_residual_init))
 print("max excess curvature (optimized):", jnp.max(coil_curvature_residual_opt))
-print("Curvature loss (initial):", loss_coil_curvature_from_field(init_field, max_coil_curvature=CURVATURE_TARGET))
-print("Curvature loss (optimized):", loss_coil_curvature_from_field(opt_field, max_coil_curvature=CURVATURE_TARGET))
+print("Curvature loss (initial):", loss_coil_curvature_from_field(field_init, max_coil_curvature=CURVATURE_TARGET))
+print("Curvature loss (optimized):", loss_coil_curvature_from_field(field_opt, max_coil_curvature=CURVATURE_TARGET))
 
 print("\n---------------------------------------------------------------------------")
 print("\nQuasi-symmetry residuals and losses:")
-QS_residual_xyz_init = quasi_symmetry_residual_on_surface(init_field, surface)
-QS_residual_xyz_opt = quasi_symmetry_residual_on_surface(opt_field, opt_surface)
+QS_residual_xyz_init = quasi_symmetry_residual_on_surface(field_init, surface_init)
+QS_residual_xyz_opt = quasi_symmetry_residual_on_surface(field_opt, surface_opt)
 print("mean abs residual (initial):", jnp.mean(jnp.abs(QS_residual_xyz_init)))
 print("mean abs residual (optimized):", jnp.mean(jnp.abs(QS_residual_xyz_opt)))
 print("max abs residual (initial):", jnp.max(jnp.abs(QS_residual_xyz_init)))
 print("max abs residual (optimized):", jnp.max(jnp.abs(QS_residual_xyz_opt)))
-print("QS loss (initial):", loss_quasi_symmetry(init_field, surface))
-print("QS loss (optimized):", loss_quasi_symmetry(opt_field, opt_surface))
+print("QS loss (initial):", loss_quasi_symmetry(field_init, surface_init))
+print("QS loss (optimized):", loss_quasi_symmetry(field_opt, surface_opt))
 
 print("\n---------------------------------------------------------------------------")
 print("\nMean cross-sectional area:")
-cross_sectional_area_initial = surface.area_section_by_phi()
-cross_sectional_area_optimized = opt_surface.area_section_by_phi()
+cross_sectional_area_initial = surface_init.area_section_by_phi()
+cross_sectional_area_optimized = surface_opt.area_section_by_phi()
 relative_cross_sectional_area_change = ( cross_sectional_area_optimized - cross_sectional_area_initial ) / cross_sectional_area_initial
 
 print("Initial area:", cross_sectional_area_initial)
@@ -247,8 +247,8 @@ print("Optimized area:", cross_sectional_area_optimized)
 print("Absolute area change:", jnp.abs(cross_sectional_area_optimized - cross_sectional_area_initial))
 print("Relative area change:", relative_cross_sectional_area_change)
 print("Relative area change (%):", 100 * relative_cross_sectional_area_change)
-print("Area loss:", loss_mean_cross_sectional_area( opt_surface , target_area=CROSS_SECTIONAL_AREA_TARGET ) )
-print("Weighted area loss:", CROSS_SECTIONAL_AREA_WEIGHT * loss_mean_cross_sectional_area( opt_surface, target_area=CROSS_SECTIONAL_AREA_TARGET ) )
+print("Area loss:", loss_mean_cross_sectional_area( surface_opt , target_area=CROSS_SECTIONAL_AREA_TARGET ) )
+print("Weighted area loss:", CROSS_SECTIONAL_AREA_WEIGHT * loss_mean_cross_sectional_area( surface_opt, target_area=CROSS_SECTIONAL_AREA_TARGET ) )
 
 
 # ====================================================================================
@@ -261,11 +261,11 @@ fig = plt.figure(figsize=(8, 4))
 
 ax1 = fig.add_subplot(121, projection='3d')
 init_coils.plot(ax=ax1, show=False)
-surface.plot(ax=ax1, show=False)
+surface_init.plot(ax=ax1, show=False)
 
 ax2 = fig.add_subplot(122, projection='3d')
 opt_coils.plot(ax=ax2, show=False)
-opt_surface.plot(ax=ax2, show=False)
+surface_opt.plot(ax=ax2, show=False)
 
 plt.tight_layout()
 plt.show()
@@ -281,7 +281,7 @@ if EXPORT:
     opt_coils.to_json(os.path.join(output_filepath, "opt_coils_vmec_surface.json"))
 
     """ Save results in vtk format to analyze in Paraview """
-    surface.to_vtk(os.path.join(output_filepath, "init_surface_vmec_surface.json"), field=init_field)
-    opt_surface.to_vtk(os.path.join(output_filepath, "final_surface_vmec_surface.json"), field=opt_field)
+    surface_init.to_vtk(os.path.join(output_filepath, "init_surface_vmec_surface.json"), field=field_init)
+    surface_opt.to_vtk(os.path.join(output_filepath, "final_surface_vmec_surface.json"), field=field_opt)
     init_coils.to_vtk(os.path.join(output_filepath, "init_coils_vmec_surface.json"))
     opt_coils.to_vtk(os.path.join(output_filepath, "opt_coils_vmec_surface.json"))
